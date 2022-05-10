1fad2e589e003d1b9725a5a3c197aa20cf82662b
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2022-05-10 03:06:15 -0400

    RearVan anim test

--- Contents/mods/CSVP/media/lua/client/P30Step_Animations.lua
+++ Contents/mods/CSVP/media/lua/client/P30Step_Animations.lua
@@ -2,39 +2,51 @@
 -- require "Vehicles/ISUI/ISVehicleMenu"
 -- require "Vehicles/TimedActions/ISStartVehicleEngine"
 
-local function IFAV_Enter(player)
+local function StepVanP30Police_enter(player)
 	local vehicle = player:getVehicle()
 	if not vehicle then return end
     local vehicleName = vehicle:getScriptName()
     local seat = vehicle:getSeat(player)
     if not seat then return end
-	if seat == 0 and vehicleName:contains("Base.IFAV") then		
-		player:SetVariable("VehicleScriptName", "Shark_Idle_Driver")
+	if seat == 0 and vehicleName:contains("Base.SC_StepVanP30Police") then		
+		player:SetVariable("VehicleScriptName", "Shark_Idle_RearVan")
 		return
 	end
-	if seat == 1 and vehicleName:contains("Base.IFAV") then		
+	if seat == 1 and vehicleName:contains("Base.SC_StepVanP30Police") then		
 		player:SetVariable("VehicleScriptName", "Shark_Idle_Passenger")
 		return
 	end
-	if seat == 2 and vehicleName:contains("Base.IFAV") then		
-		player:SetVariable("VehicleScriptName", "Shark_Idle_RearPassenger")
+	if seat == 2 and vehicleName:contains("Base.SC_StepVanP30Police") then		
+		player:SetVariable("VehicleScriptName", "Shark_Idle_RearVan")
 		return
 	end
-	if seat == 3 and vehicleName:contains("Base.IFAV") then		
-		player:SetVariable("VehicleScriptName", "Shark_Idle_RearPassenger")
+	if seat == 3 and vehicleName:contains("Base.SC_StepVanP30Police") then		
+		player:SetVariable("VehicleScriptName", "Shark_Idle_RearVan")
+		return
+	end
+	if seat == 4 and vehicleName:contains("Base.SC_StepVanP30Police") then		
+		player:SetVariable("VehicleScriptName", "Shark_Idle_RearVan")
+		return
+	end
+	if seat == 5 and vehicleName:contains("Base.SC_StepVanP30Police") then		
+		player:SetVariable("VehicleScriptName", "Shark_Idle_RearVan")
+		return
+	end
+	if seat == 6 and vehicleName:contains("Base.SC_StepVanP30Police") then		
+		player:SetVariable("VehicleScriptName", "Shark_Idle_RearVan")
 		return
 	end
 end
 
-function IFAV_Enter_Server(player)
-	IFAV_Enter(player)
+function SC_StepVanP30Police_Enter_Server(player)
+	SC_StepVanP30Police_Enter(player)
 end
 
-local function IFAV_Exit(player)
-    sendClientCommand(player, "IFAV", "PlayerExit", {})
+local function SC_StepVanP30Police_Exit(player)
+    sendClientCommand(player, "SC_StepVanP30Police", "PlayerExit", {})
     player:SetVariable("VehicleScriptName", "")
 end
 
-Events.OnEnterVehicle.Add(IFAV_Enter)
-Events.OnExitVehicle.Add(IFAV_Exit)
-Events.OnSwitchVehicleSeat.Add(IFAV_Enter)
+Events.OnEnterVehicle.Add(SC_StepVanP30Police_Enter)
+Events.OnExitVehicle.Add(SC_StepVanP30Police_Exit)
+Events.OnSwitchVehicleSeat.Add(SC_StepVanP30Police_Enter)
