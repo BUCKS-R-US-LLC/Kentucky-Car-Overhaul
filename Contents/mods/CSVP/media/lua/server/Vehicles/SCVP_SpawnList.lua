5f329a1e36ee4b04ae8167c0a0121edbf6ccca5c
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2022-05-17 22:00:55 -0400

    Update SCVP_SpawnList.lua

--- Contents/mods/CSVP/media/lua/server/Vehicles/SCVP_SpawnList.lua
+++ Contents/mods/CSVP/media/lua/server/Vehicles/SCVP_SpawnList.lua
@@ -5,7 +5,7 @@ if VehicleZoneDistribution then
     VehicleZoneDistribution.trafficjamw.vehicles["Base.SC_StepVanP30Police"] = {index = -1, spawnChance = 5};
     VehicleZoneDistribution.trafficjamw.vehicles["Base.SC_CrownVictoriaStatePolice"] = {index = -1, spawnChance = 50};
     VehicleZoneDistribution.trafficjamw.vehicles["Base.SC_M1009MilitaryPolice"] = {index = -1, spawnChance = 10};
-    VehicleZoneDistribution.trafficjamw.spawnRate = 50;
+    VehicleZoneDistribution.trafficjamw.spawnRate = 10;
     
     VehicleZoneDistribution.police.vehicles["Base.SC_K5BlazerStatePolice"] = {index = -1, spawnChance = 70};
     VehicleZoneDistribution.police.vehicles["Base.SC_Caprice9C1StatePolice"] = {index = -1, spawnChance = 60};
@@ -25,6 +25,7 @@ if VehicleZoneDistribution then
     VehicleZoneDistribution.medium.vehicles["Base.SC_Caprice9C1StatePolice"] = {index = -1, spawnChance = 10};
     VehicleZoneDistribution.medium.vehicles["Base.SC_K5BlazerParkRanger"] = {index = -1, spawnChance = 10};
     VehicleZoneDistribution.medium.vehicles["Base.SC_CrownVictoriaParkRanger"] = {index = -1, spawnChance = 10};
+    VehicleZoneDistribution.medium.vehicles["Base.SC_CrownVictoriaLVPD"] = {index = -1, spawnChance = 1};
 
     VehicleZoneDistribution.good.vehicles["Base.SC_K5BlazerParkRanger"] = {index = -1, spawnChance = 5};
     VehicleZoneDistribution.good.vehicles["Base.SC_Caprice9C1ParkRanger"] = {index = -1, spawnChance = 5};    
@@ -32,6 +33,7 @@ if VehicleZoneDistribution then
     VehicleZoneDistribution.good.vehicles["Base.SC_Crown9C1StatePolice"] = {index = -1, spawnChance = 10};
     VehicleZoneDistribution.good.vehicles["Base.SC_K5BlazerParkRanger"] = {index = -1, spawnChance = 5};
     VehicleZoneDistribution.good.vehicles["Base.SC_Caprice9C1ParkRanger"] = {index = -1, spawnChance = 5};
+    VehicleZoneDistribution.good.vehicles["Base.SC_Caprice9C1LVPD"] = {index = -1, spawnChance = 1};
 
     VehicleZoneDistribution.military = VehicleZoneDistribution.military or {}
     VehicleZoneDistribution.military.vehicles = VehicleZoneDistribution.military.vehicles or {}
