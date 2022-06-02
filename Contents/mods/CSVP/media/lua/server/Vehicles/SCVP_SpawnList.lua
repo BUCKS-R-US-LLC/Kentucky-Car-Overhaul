2233f5fe47a8d3a73e10cc2178b717b926d45faf
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2022-06-02 15:30:33 -0400

    Update SCVP_SpawnList.lua

--- Contents/mods/CSVP/media/lua/server/Vehicles/SCVP_SpawnList.lua
+++ Contents/mods/CSVP/media/lua/server/Vehicles/SCVP_SpawnList.lua
@@ -1,10 +1,9 @@
 if VehicleZoneDistribution then
 
     VehicleZoneDistribution.parkingstall.vehicles["Base.SC_K5BlazerCiv"] = {index = -1, spawnChance = 5};
-    VehicleZoneDistribution.parkingstall.vehicles["Base.SC_CrownVictoriaCiv"] = {index = -1, spawnChance = 5};
-    VehicleZoneDistribution.parkingstall.vehicles["Base.SC_CapriceCiv"] = {index = -1, spawnChance = 5};
-    VehicleZoneDistribution.parkingstall.vehicles["Base.SC_CrownVictoriaTaxi"] = {index = -1, spawnChance = 5};
-    VehicleZoneDistribution.parkingstall.vehicles["Base.SC_FordCountrySquire"] = {index = -1, spawnChance = 5};
+    VehicleZoneDistribution.parkingstall.vehicles["Base.SC_CrownVictoriaCiv"] = {index = -1, spawnChance = 20};
+    VehicleZoneDistribution.parkingstall.vehicles["Base.SC_CapriceCiv"] = {index = -1, spawnChance = 15};
+    VehicleZoneDistribution.parkingstall.vehicles["Base.SC_CrownVictoriaTaxi"] = {index = -1, spawnChance = 10};
     VehicleZoneDistribution.parkingstall.vehicles["Base.SC_P30VanCiv"] = {index = -1, spawnChance = 5};
     VehicleZoneDistribution.parkingstall.vehicles["Base.SC_G30Van"] = {index = -1, spawnChance = 1};
     VehicleZoneDistribution.parkingstall.chanceToPartDamage = 5;
@@ -29,33 +28,33 @@ if VehicleZoneDistribution then
     VehicleZoneDistribution.police.spawnRate = 30;
     VehicleZoneDistribution.police.chanceToPartDamage = 5;
 
-    VehicleZoneDistribution.ranger.vehicles["Base.SC_K5BlazerParkRanger"] = {index = -1, spawnChance = 5};
-    VehicleZoneDistribution.ranger.vehicles["Base.SC_Caprice9C1ParkRanger"] = {index = -1, spawnChance = 3};
-    VehicleZoneDistribution.ranger.vehicles["Base.SC_StepVanP30ParkRanger"] = {index = -1, spawnChance = 5};
-    VehicleZoneDistribution.ranger.vehicles["Base.SC_CrownVictoriaParkRanger"] = {index = -1, spawnChance = 3};
+    VehicleZoneDistribution.ranger.vehicles["Base.SC_K5BlazerParkRanger"] = {index = -1, spawnChance = 50};
+    VehicleZoneDistribution.ranger.vehicles["Base.SC_Caprice9C1ParkRanger"] = {index = -1, spawnChance = 50};
+    VehicleZoneDistribution.ranger.vehicles["Base.SC_StepVanP30ParkRanger"] = {index = -1, spawnChance = 1};
+    VehicleZoneDistribution.ranger.vehicles["Base.SC_CrownVictoriaParkRanger"] = {index = -1, spawnChance = 10};
     VehicleZoneDistribution.ranger.vehicles["Base.SC_M1009MilitaryPolice"] = {index = -1, spawnChance = 5};
     VehicleZoneDistribution.ranger.chanceToPartDamage = 5;
     
-    VehicleZoneDistribution.medium.vehicles["Base.SC_K5BlazerCiv"] = {index = -1, spawnChance = 5};
-    VehicleZoneDistribution.medium.vehicles["Base.SC_CrownVictoriaCiv"] = {index = -1, spawnChance = 5};
+    VehicleZoneDistribution.medium.vehicles["Base.SC_K5BlazerCiv"] = {index = -1, spawnChance = 10};
+    VehicleZoneDistribution.medium.vehicles["Base.SC_CrownVictoriaCiv"] = {index = -1, spawnChance = 10};
     VehicleZoneDistribution.medium.vehicles["Base.SC_CapriceCiv"] = {index = -1, spawnChance = 10};
     VehicleZoneDistribution.medium.vehicles["Base.SC_P30VanCiv"] = {index = -1, spawnChance = 5};
     VehicleZoneDistribution.medium.vehicles["Base.SC_K5BlazerStatePolice"] = {index = -1, spawnChance = 10};
     VehicleZoneDistribution.medium.vehicles["Base.SC_Caprice9C1StatePolice"] = {index = -1, spawnChance = 10};
     VehicleZoneDistribution.medium.vehicles["Base.SC_K5BlazerParkRanger"] = {index = -1, spawnChance = 10};
-    VehicleZoneDistribution.medium.vehicles["Base.SC_CrownVictoriaParkRanger"] = {index = -1, spawnChance = 1};
+    VehicleZoneDistribution.medium.vehicles["Base.SC_CrownVictoriaParkRanger"] = {index = -1, spawnChance = 10};
     VehicleZoneDistribution.medium.vehicles["Base.SC_CrownVictoriaLVPD"] = {index = -1, spawnChance = 1};
-    VehicleZoneDistribution.medium.vehicles["Base.SC_CrownVictoriaTaxi"] = {index = -1, spawnChance = 5};
+    VehicleZoneDistribution.medium.vehicles["Base.SC_CrownVictoriaTaxi"] = {index = -1, spawnChance = 10};
     VehicleZoneDistribution.medium.vehicles["Base.SC_K5BlazerCiv"] = {index = -1, spawnChance = 10};
-    VehicleZoneDistribution.medium.vehicles["Base.SC_G30Van"] = {index = -1, spawnChance = 5};
+    VehicleZoneDistribution.medium.vehicles["Base.SC_G30Van"] = {index = -1, spawnChance = 10};
     VehicleZoneDistribution.medium.chanceToPartDamage = 10;
 
     VehicleZoneDistribution.good.vehicles["Base.SC_P30VanCiv"] = {index = -1, spawnChance = 5};
-    VehicleZoneDistribution.good.vehicles["Base.SC_CrownVictoriaTaxi"] = {index = -1, spawnChance = 5};
+    VehicleZoneDistribution.good.vehicles["Base.SC_CrownVictoriaTaxi"] = {index = -1, spawnChance = 10};
     VehicleZoneDistribution.good.vehicles["Base.SC_K5BlazerParkRanger"] = {index = -1, spawnChance = 5};
     VehicleZoneDistribution.good.vehicles["Base.SC_Caprice9C1ParkRanger"] = {index = -1, spawnChance = 5};    
     VehicleZoneDistribution.good.vehicles["Base.SC_CrownVictoriaStatePolice"] = {index = -1, spawnChance = 10};
-    VehicleZoneDistribution.good.vehicles["Base.SC_Crown9C1StatePolice"] = {index = -1, spawnChance = 5};
+    VehicleZoneDistribution.good.vehicles["Base.SC_Crown9C1StatePolice"] = {index = -1, spawnChance = 10};
     VehicleZoneDistribution.good.vehicles["Base.SC_K5BlazerParkRanger"] = {index = -1, spawnChance = 5};
     VehicleZoneDistribution.good.vehicles["Base.SC_Caprice9C1ParkRanger"] = {index = -1, spawnChance = 5};
     VehicleZoneDistribution.good.vehicles["Base.SC_Caprice9C1LVPD"] = {index = -1, spawnChance = 1};
