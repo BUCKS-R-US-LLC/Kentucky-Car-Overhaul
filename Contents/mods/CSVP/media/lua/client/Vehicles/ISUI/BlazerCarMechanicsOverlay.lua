ea1584a62a907b0854f17f7e42b129f2228c311b
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2022-06-14 23:20:52 -0400

    M1009 stuff

--- Contents/mods/CSVP/media/lua/client/Vehicles/ISUI/BlazerCarMechanicsOverlay.lua
+++ Contents/mods/CSVP/media/lua/client/Vehicles/ISUI/BlazerCarMechanicsOverlay.lua
@@ -1,4 +1,5 @@
 ISCarMechanicsOverlay.CarList["Base.SC_M1009MilitaryPolice"] = {imgPrefix = "blazer_", x=0,y=0};
+ISCarMechanicsOverlay.CarList["Base.SC_M1009"] = {imgPrefix = "blazer_", x=0,y=0};
 ISCarMechanicsOverlay.CarList["Base.SC_K5BlazerParkRanger"] = {imgPrefix = "blazer_", x=0,y=0};
 ISCarMechanicsOverlay.CarList["Base.SC_K5BlazerStatePolice"] = {imgPrefix = "blazer_", x=0,y=0};
 ISCarMechanicsOverlay.CarList["Base.SC_K5BlazerCiv"] = {imgPrefix = "blazer_", x=0,y=0};
