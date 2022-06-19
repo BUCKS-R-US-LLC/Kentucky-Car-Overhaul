113e2d079dda0f7e076b292f74659db44b1e6ef8
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2022-06-19 06:42:33 -0400

    Zoning stuff

---
+++ Contents/mods/CSVP/media/lua/maps/SCUCK/objects.lua
@@ -0,0 +1,10 @@
+objects = {
+  { name = "SCKMilitary", type = "ParkingStall", x = 12019, y = 1327, z = 0, width = 5, height = 3 },
+  { name = "SCKMilitary", type = "ParkingStall", x = 12019, y = 1330, z = 0, width = 5, height = 3 },
+  { name = "SCKMilitary", type = "ParkingStall", x = 12019, y = 1333, z = 0, width = 5, height = 3 },
+  { name = "SCKMilitary", type = "ParkingStall", x = 12060, y = 1411, z = 0, width = 3, height = 5 },
+  { name = "SCKMilitary", type = "ParkingStall", x = 12063, y = 1411, z = 0, width = 3, height = 5 },
+  { name = "SCKMilitary", type = "ParkingStall", x = 12066, y = 1411, z = 0, width = 3, height = 5 },
+  { name = "SCKMilitary", type = "ParkingStall", x = 12099, y = 1450, z = 0, width = 5, height = 3 },
+  { name = "SCKMilitary", type = "ParkingStall", x = 12099, y = 1423, z = 0, width = 5, height = 3 },
+}
