8d293c691ef2eb303e1fef6d1ccb6eb544d8f2af
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2022-07-01 12:32:12 -0400

    Added Chuck's new animation code

--- Contents/mods/SCKCO - Core/media/lua/client/SC_Animations.lua
+++ Contents/mods/SCKCO - Core/media/lua/client/SC_Animations.lua
@@ -1,113 +1,92 @@
 -- Huge thanks to Dislaik for originally creating this method (which we added a change to)
 -- You should him out: https://steamcommunity.com/sharedfiles/filedetails/?id=2728300240
 
-local function CapriceCiv_Enter(player)
+
+---If the provided 'input' is not a table then that is returned (string expected).
+---Otherwise; then parse every other value as a acceptable value and it's weight.
+local function parseSeatAnimationSelection(input)
+	if type(input) == "table" then
+		---If input is a table parse each value and check if it's a string
+		local parsedWeightedInput = {}
+		for key,value in pairs(input) do
+			if type(value) == "string" then
+				---If the value is a string then check the next value
+				---If it's a number use as an iterator; otherwise use 1
+				local iterations = input[key+1]
+				if type(iterations) ~= "number" then
+					iterations = 1
+				end
+				---Add as many iterations as selected for value
+				for i=1, iterations do
+					table.insert(parsedWeightedInput, value)
+				end
+			end
+		end
+
+		if #parsedWeightedInput>0 then
+			local selection = parsedWeightedInput[ZombRand(#parsedWeightedInput)+1]
+			return selection
+		else
+			print("SC_ANIM: DEBUG ERROR: parsedWeightedInput not populated. (input:"..tostring(input)..")")
+		end
+	else
+		return input
+	end
+end
+
+
+local vehicleToSeatAnimations = {
+
+    ["DEFAULT"] = {
+        ["seat0"]="Shark_Idle_Driver",
+        ["passenger"]="Shark_Idle_Passenger",
+    },
+
+    ["Base.SC_CapriceCiv"] = {
+        ["seat0"]="Shark_Idle_Driver",
+        ["seat1"]="Shark_Idle_Passenger",
+        ["passenger"]={"Shark_Idle_Driver","Shark_Idle_Passenger",99},
+        },
+
+    ["Base.SC_StepVanP30Police"] = {
+        ["seat0"]="Shark_Idle_Driver",
+        ["seat0"]="Shark_Idle_Passenger",
+        ["passenger"]="Shark_Idle_RearVan",
+    }
+
+}
+
+---@param player IsoPlayer|IsoGameCharacter
+local function Vehicle_Enter(player)
+	---@type BaseVehicle
 	local vehicle = player:getVehicle()
 	if not vehicle then return end
-    local vehicleName = vehicle:getScriptName()
-    local seat = vehicle:getSeat(player)
-    if not seat then return end
-	if seat == 0 and vehicleName:contains("Base.SC_CapriceCiv") then				
-		player:SetVariable("VehicleScriptName", "Shark_Idle_Driver")
-		return
-	end
-	if seat == 1 and vehicleName:contains("Base.SC_CapriceCiv") then
---- Below sets random chance between if and else
-		if ZombRand(100) <= 50 then		
-		player:SetVariable("VehicleScriptName", "Shark_Idle_Driver")
-		else
-		player:SetVariable("VehicleScriptName", "Shark_Idle_Passenger")
-		return
-		end
-	end
-	if seat == 2 and vehicleName:contains("Base.SC_CapriceCiv") then		
---- Below sets random chance between if and else
-		if ZombRand(100) <= 50 then		
-		player:SetVariable("VehicleScriptName", "Shark_Idle_Driver")
-		else
-		player:SetVariable("VehicleScriptName", "Shark_Idle_Passenger")
-		return
-		end
-	end
-	if seat == 3 and vehicleName:contains("Base.SC_CapriceCiv") then		
---- Below sets random chance between if and else
-		if ZombRand(100) <= 50 then		
-		player:SetVariable("VehicleScriptName", "Shark_Idle_Driver")
-		else
-		player:SetVariable("VehicleScriptName", "Shark_Idle_Passenger")
-		return
-		end
-	end
-	if seat == 4 and vehicleName:contains("Base.SC_CapriceCiv") then		
---- Below sets random chance between if and else
-		if ZombRand(100) <= 50 then		
-		player:SetVariable("VehicleScriptName", "Shark_Idle_Driver")
-		else
-		player:SetVariable("VehicleScriptName", "Shark_Idle_Passenger")
-		return
-		end
+
+	local vehicleName = vehicle:getScriptName()
+	print("SC_ANIM: DEBUG: "..vehicleName)
+
+	local seat = vehicle:getSeat(player)
+	print(" -- Seat: "..seat)
+	if not seat then return end
+
+	local vehicleAnimation = vehicleToSeatAnimations[vehicleName] or vehicleToSeatAnimations["DEFAULT"]
+
+	local fetchedAnimation = vehicleAnimation["seat"..seat] or vehicleAnimation["passenger"]
+	fetchedAnimation = parseSeatAnimationSelection(fetchedAnimation)
+	print(" ---- Anim Selected: "..fetchedAnimation)
+
+	if fetchedAnimation then
+		player:SetVariable("VehicleScriptName", fetchedAnimation)
 	end
 end
 
-function CapriceCiv_Enter_Server(player)
-	CapricePolice_Enter(player)
-end
 
-local function CapriceCiv_Exit(player)
-    sendClientCommand(player, "SC_CapriceCiv", "PlayerExit", {})
+Events.OnEnterVehicle.Add(Vehicle_Enter)
+Events.OnSwitchVehicleSeat.Add(Vehicle_Enter)
+
+
+local function Vehicle_Exit(player)
+    --sendClientCommand(player, "SC_VehicleAnim", "PlayerExit", {})
     player:SetVariable("VehicleScriptName", "")
 end
-
-Events.OnEnterVehicle.Add(CapriceCiv_Enter)
-Events.OnExitVehicle.Add(CapriceCiv_Exit)
-Events.OnSwitchVehicleSeat.Add(CapriceCiv_Enter)
-
-
-local function StepVanP30Police_enter(player)
-	local vehicle = player:getVehicle()
-	if not vehicle then return end
-    local vehicleName = vehicle:getScriptName()
-    local seat = vehicle:getSeat(player)
-    if not seat then return end
-	if seat == 0 and vehicleName:contains("Base.SC_StepVanP30Police") then		
-		player:SetVariable("VehicleScriptName", "Shark_Idle_RearVan")
-		return
-	end
-	if seat == 1 and vehicleName:contains("Base.SC_StepVanP30Police") then		
-		player:SetVariable("VehicleScriptName", "Shark_Idle_Passenger")
-		return
-	end
-	if seat == 2 and vehicleName:contains("Base.SC_StepVanP30Police") then		
-		player:SetVariable("VehicleScriptName", "Shark_Idle_RearVan")
-		return
-	end
-	if seat == 3 and vehicleName:contains("Base.SC_StepVanP30Police") then		
-		player:SetVariable("VehicleScriptName", "Shark_Idle_RearVan")
-		return
-	end
-	if seat == 4 and vehicleName:contains("Base.SC_StepVanP30Police") then		
-		player:SetVariable("VehicleScriptName", "Shark_Idle_RearVan")
-		return
-	end
-	if seat == 5 and vehicleName:contains("Base.SC_StepVanP30Police") then		
-		player:SetVariable("VehicleScriptName", "Shark_Idle_RearVan")
-		return
-	end
-	if seat == 6 and vehicleName:contains("Base.SC_StepVanP30Police") then		
-		player:SetVariable("VehicleScriptName", "Shark_Idle_RearVan")
-		return
-	end
-end
-
-function SC_StepVanP30Police_Enter_Server(player)
-	SC_StepVanP30Police_Enter(player)
-end
-
-local function SC_StepVanP30Police_Exit(player)
-    sendClientCommand(player, "SC_StepVanP30Police", "PlayerExit", {})
-    player:SetVariable("VehicleScriptName", "")
-end
-
-Events.OnEnterVehicle.Add(SC_StepVanP30Police_Enter)
-Events.OnExitVehicle.Add(SC_StepVanP30Police_Exit)
-Events.OnSwitchVehicleSeat.Add(SC_StepVanP30Police_Enter)
+Events.OnExitVehicle.Add(Vehicle_Exit)
