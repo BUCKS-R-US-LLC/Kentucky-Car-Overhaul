eec7b0efe19db278b841d2bf05561f1b73f23419
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2022-07-06 21:54:05 -0400

    Delete SC_containers.lua

--- Contents/mods/SCKCO - Core/media/lua/server/SC_containers.lua
+++
@@ -1,9 +0,0 @@
-require "Vehicle/Vehicles"
-
--- TO DO: Learn not to just paste functions over and over
-
-function Vehicles.ContainerAccess.SCInteriorArmory(vehicle, part, chr)
-    if chr:getVehicle() == vehicle then
-        return true
-    end
-end
