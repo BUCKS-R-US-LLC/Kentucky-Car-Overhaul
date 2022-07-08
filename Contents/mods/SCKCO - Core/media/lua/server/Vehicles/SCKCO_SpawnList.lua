0cf02e61bdb23771c592ea986a185f70af0cedd0
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2022-07-08 14:54:07 -0400

    Update SCKCO_SpawnList.lua

--- Contents/mods/SCKCO - Core/media/lua/server/Vehicles/SCKCO_SpawnList.lua
+++ Contents/mods/SCKCO - Core/media/lua/server/Vehicles/SCKCO_SpawnList.lua
@@ -11,7 +11,6 @@ if VehicleZoneDistribution then
     VehicleZoneDistribution.parkingstall.vehicles["Base.SC_G30VanWindowless"] = {index = -1, spawnChance = 5};
     VehicleZoneDistribution.parkingstall.vehicles["Base.SC_G30VanNormal"] = {index = -1, spawnChance = 1};
     VehicleZoneDistribution.parkingstall.vehicles["Base.SC_JeepCJ7"] = {index = -1, spawnChance = 5};
-    VehicleZoneDistribution.parkingstall.chanceToPartDamage = 5;
 
     VehicleZoneDistribution.trafficjamw.vehicles["Base.SC_K10Civ"] = {index = -1, spawnChance = 10};
     VehicleZoneDistribution.trafficjamw.vehicles["Base.SC_K5BlazerCiv"] = {index = -1, spawnChance = 10};
@@ -29,8 +28,10 @@ if VehicleZoneDistribution then
     VehicleZoneDistribution.trafficjamw.vehicles["Base.SC_M1009MilitaryPolice"] = {index = -1, spawnChance = 10};
     VehicleZoneDistribution.trafficjamw.vehicles["Base.SC_G30VanWindowless"] = {index = -1, spawnChance = 5};
     VehicleZoneDistribution.trafficjamw.vehicles["Base.SC_G30VanNormal"] = {index = -1, spawnChance = 5};
+    VehicleZoneDistribution.trafficjamw.vehicles["Base.SC_G30BoxVan"] = {index = -1, spawnChance = 25};
     VehicleZoneDistribution.trafficjamw.vehicles["Base.SC_JeepCJ7"] = {index = -1, spawnChance = 5};
     VehicleZoneDistribution.trafficjamw.vehicles["Base.SC_CamaroTTop"] = {index = -1, spawnChance = 5};
+    VehicleZoneDistribution.trafficjamw.vehicles["Base.SC_Mercedes280"] = {index = -1, spawnChance = 5};
     VehicleZoneDistribution.trafficjamw.spawnRate = 10;
 
     VehicleZoneDistribution.trafficjame.vehicles["Base.SC_K10Civ"] = {index = -1, spawnChance = 10};
@@ -51,6 +52,7 @@ if VehicleZoneDistribution then
     VehicleZoneDistribution.trafficjame.vehicles["Base.SC_G30VanNormal"] = {index = -1, spawnChance = 5};
     VehicleZoneDistribution.trafficjame.vehicles["Base.SC_JeepCJ7"] = {index = -1, spawnChance = 5};
     VehicleZoneDistribution.trafficjame.vehicles["Base.SC_CamaroTTop"] = {index = -1, spawnChance = 5};
+    VehicleZoneDistribution.trafficjame.vehicles["Base.SC_G30BoxVan"] = {index = -1, spawnChance = 25};
     VehicleZoneDistribution.trafficjame.spawnRate = 10;
 
     VehicleZoneDistribution.trafficjamn.vehicles["Base.SC_K10Civ"] = {index = -1, spawnChance = 10};
@@ -71,11 +73,12 @@ if VehicleZoneDistribution then
     VehicleZoneDistribution.trafficjamn.vehicles["Base.SC_G30VanNormal"] = {index = -1, spawnChance = 5};
     VehicleZoneDistribution.trafficjamn.vehicles["Base.SC_JeepCJ7"] = {index = -1, spawnChance = 5};
     VehicleZoneDistribution.trafficjamn.vehicles["Base.SC_CamaroTTop"] = {index = -1, spawnChance = 5};
+    VehicleZoneDistribution.trafficjamn.vehicles["Base.SC_G30BoxVan"] = {index = -1, spawnChance = 25};
     VehicleZoneDistribution.trafficjamn.spawnRate = 10;
     
     VehicleZoneDistribution.police.vehicles["Base.SC_K5BlazerStatePolice"] = {index = -1, spawnChance = 70};
     VehicleZoneDistribution.police.vehicles["Base.SC_Caprice9C1StatePolice"] = {index = -1, spawnChance = 60};
-    VehicleZoneDistribution.police.vehicles["Base.SC_StepVanP30Police"] = {index = -1, spawnChance = 10};
+    VehicleZoneDistribution.police.vehicles["Base.SC_StepVanP30Police"] = {index = -1, spawnChance = 1};
     VehicleZoneDistribution.police.vehicles["Base.SC_CrownVictoriaStatePolice"] = {index = -1, spawnChance = 60};
     VehicleZoneDistribution.police.spawnRate = 30;
     VehicleZoneDistribution.police.chanceToPartDamage = 5;
@@ -102,8 +105,7 @@ if VehicleZoneDistribution then
     VehicleZoneDistribution.medium.vehicles["Base.SC_G30VanWindowless"] = {index = -1, spawnChance = 5};
     VehicleZoneDistribution.medium.vehicles["Base.SC_G30VanNormal"] = {index = -1, spawnChance = 1};
     VehicleZoneDistribution.medium.vehicles["Base.SC_JeepCJ7"] = {index = -1, spawnChance = 5};
-    VehicleZoneDistribution.massgenfac.vehicles["Base.SC_G30BoxVan"] = {index = -1, spawnChance = 5};
-    VehicleZoneDistribution.medium.chanceToPartDamage = 10;
+    VehicleZoneDistribution.medium.chanceToPartDamage = 20;
 
     VehicleZoneDistribution.good.vehicles["Base.SC_K10Civ"] = {index = -1, spawnChance = 5};
     VehicleZoneDistribution.good.vehicles["Base.SC_P30VanCiv"] = {index = -1, spawnChance = 5};
@@ -118,9 +120,11 @@ if VehicleZoneDistribution then
     VehicleZoneDistribution.good.vehicles["Base.SC_G30VanNormal"] = {index = -1, spawnChance = 1};
     VehicleZoneDistribution.good.vehicles["Base.SC_JeepCJ7"] = {index = -1, spawnChance = 5};
     VehicleZoneDistribution.good.vehicles["Base.SC_CamaroTTop"] = {index = -1, spawnChance = 5};
-    VehicleZoneDistribution.good.chanceToPartDamage = 5;
+    VehicleZoneDistribution.good.vehicles["Base.SC_Mercedes280"] = {index = -1, spawnChance = 5};
+    VehicleZoneDistribution.good.chanceToPartDamage = 15;
 
     VehicleZoneDistribution.sport.vehicles["Base.SC_CamaroTTop"] = {index = -1, spawnChance = 5};
+    VehicleZoneDistribution.medium.chanceToPartDamage = 5;
 
     VehicleZoneDistribution.postal.vehicles["Base.SC_G30BoxVan"] = {index = -1, spawnChance = 25};
     VehicleZoneDistribution.postal.vehicles["Base.SC_G30Mail"] = {index = -1, spawnChance = 25};
