d80be9f933ee9c0ed9a6e711f3c22bea0d47dca3
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2022-07-24 14:15:43 -0400

    Added SLEO support

--- Contents/mods/SCKCO - Core/media/lua/server/Vehicles/SCKCO_VehicleDistro.lua
+++ Contents/mods/SCKCO - Core/media/lua/server/Vehicles/SCKCO_VehicleDistro.lua
@@ -111,10 +111,17 @@ VehicleDistributions.SCMilitaryPolice = {
     SeatRearRight = VehicleDistributions.Seat;
 }
 
-VehicleDistributions.SCPolice = {
+VehicleDistributions.SCPolicePatrol = {
     TruckBed = {
         rolls = 4,
         items = {
+
+            -- SLEO --
+
+            "SLEOClothing.Bag_DuffelPolice", 10,
+
+            -- Vanilla -- 
+
             "AmmoStrap_Bullets", 4,
             "AmmoStrap_Shells", 4,
             "AssaultRifle2", 1,
@@ -122,10 +129,7 @@ VehicleDistributions.SCPolice = {
             "Bullets9mmBox", 10,
             "HandTorch", 4,
             "Hat_CrashHelmet_Police", 2,
-            "HolsterDouble", 1,
             "HolsterSimple", 4,
-            "HuntingRifle", 8,
-            "M14Clip", 4,
             "Nightstick", 2,
             "Pistol", 10,
             "Radio.WalkieTalkie4", 10,
@@ -137,7 +141,6 @@ VehicleDistributions.SCPolice = {
             rolls = 1,
             items = {
                 "FirstAidKit", 20,
-                "Radio.HamRadio1", 20,
             }
         }
     },
@@ -187,6 +190,97 @@ VehicleDistributions.SCPolice = {
     SeatRearRight = VehicleDistributions.Seat;
 }
 
+VehicleDistributions.SCPoliceTacticalBlue = {
+    TruckBed = {
+        rolls = 10,
+        items = {
+
+            -- SLEO --
+            "SLEOClothing.Hat_PatrolCapPolice", 4,
+            "SLEOClothing.PoliceTacticalVest", 5,
+            "SLEOClothing.Hat_PoliceHelmet", 10,
+            "SLEOClothing.Jacket_PoliceTactical", 10,
+            "SLEOClothing.Trousers_PoliceTactical", 10,
+            "SLEOClothing.Shoes_TacticalBoots", 5,
+            "SLEOClothing.Gloves_TacticalGloves", 10,
+
+            -- Vanilla -- 
+
+            "AmmoStrap_Bullets", 4,
+            "AmmoStrap_Shells", 4,
+            "AssaultRifle2", 1,
+            "Bullets9mmBox", 20,
+            "Bullets9mmBox", 10,
+            "HandTorch", 4,
+            "Hat_CrashHelmet_Police", 2,
+            "HolsterDouble", 1,
+            "HolsterSimple", 4,
+            "HuntingRifle", 8,
+            "M14Clip", 4,
+            "Nightstick", 2,
+            "Pistol", 10,
+            "Radio.WalkieTalkie4", 10,
+            "Shotgun", 10,
+            "ShotgunShellsBox", 10,
+        },
+        junk = {
+            rolls = 1,
+            items = {
+                "FirstAidKit", 20,
+            }
+        }
+    },
+    
+    SeatRearLeft = VehicleDistributions.Seat;
+    SeatRearRight = VehicleDistributions.Seat;
+}
+
+VehicleDistributions.SCPoliceTacticalGreen = {
+    TruckBed = {
+        rolls = 10,
+        items = {
+
+            -- SLEO --
+            "SLEOClothing.Hat_PatrolCapSheriff", 4,
+            "SLEOClothing.PoliceTacticalVest", 5,
+            "SLEOClothing.Hat_PoliceHelmetGreen", 10,
+            "SLEOClothing.Jacket_SheriffTactical", 10,
+            "SLEOClothing.Trousers_SheriffTactical", 10,
+            "SLEOClothing.Shoes_TacticalBoots", 5,
+            "SLEOClothing.Gloves_TacticalGloves", 10,
+
+            -- Vanilla -- 
+
+            "AmmoStrap_Bullets", 4,
+            "AmmoStrap_Shells", 4,
+            "AssaultRifle2", 1,
+            "Bullets9mmBox", 20,
+            "Bullets9mmBox", 10,
+            "HandTorch", 4,
+            "Hat_CrashHelmet_Police", 2,
+            "HolsterDouble", 1,
+            "HolsterSimple", 4,
+            "HuntingRifle", 8,
+            "M14Clip", 4,
+            "Nightstick", 2,
+            "Pistol", 10,
+            "Radio.WalkieTalkie4", 10,
+            "Shotgun", 10,
+            "ShotgunShellsBox", 10,
+        },
+        junk = {
+            rolls = 1,
+            items = {
+                "FirstAidKit", 20,
+            }
+        }
+    },
+    
+    SeatRearLeft = VehicleDistributions.Seat;
+    SeatRearRight = VehicleDistributions.Seat;
+}
+
+
 VehicleDistributions.SCCivilianGloveBox = {
     rolls = 15,
     items ={
@@ -221,19 +315,19 @@ VehicleDistributions.SCCivilian = {
 	TruckBed = VehicleDistributions.SCCivilianMisc;
 }
 
-distributionTable["SC_BlazerK5StatePolice"] = { Normal = VehicleDistributions.SCPolice; }
-distributionTable["SC_BlazerK5LVPD"] = { Normal = VehicleDistributions.SCPolice; }
-distributionTable["SC_BlazerK5ParkRanger"] = { Normal = VehicleDistributions.SCPolice; }
+distributionTable["SC_BlazerK5StatePolice"] = { Normal = VehicleDistributions.SCPolicePatrol; }
+distributionTable["SC_BlazerK5LVPD"] = { Normal = VehicleDistributions.SCPolicePatrol; }
+distributionTable["SC_BlazerK5ParkRanger"] = { Normal = VehicleDistributions.SCPolicePatrol; }
 
-distributionTable["SC_Caprice9C1StatePolice"] = { Normal = VehicleDistributions.SCPolice; }
-distributionTable["SC_Caprice9C1LVPD"] = { Normal = VehicleDistributions.SCPolice; }
-distributionTable["SC_Caprice9C1ParkRanger"] = { Normal = VehicleDistributions.SCPolice; }
+distributionTable["SC_Caprice9C1StatePolice"] = { Normal = VehicleDistributions.SCPolicePatrol; }
+distributionTable["SC_Caprice9C1LVPD"] = { Normal = VehicleDistributions.SCPolicePatrol; }
+distributionTable["SC_Caprice9C1ParkRanger"] = { Normal = VehicleDistributions.SCPolicePatrol; }
 
-distributionTable["SC_CrownVictoriaStatePolice"] = { Normal = VehicleDistributions.SCPolice; }
-distributionTable["SC_CrownVictoriaLVPD"] = { Normal = VehicleDistributions.SCPolice; }
-distributionTable["SC_CrownVictoriaParkRanger"] = { Normal = VehicleDistributions.SCPolice; }
-distributionTable["SC_StepVanP30Police"] = { Normal = VehicleDistributions.SCPolice; }
-distributionTable["SC_StepVanP30ParkRanger"] = { Normal = VehicleDistributions.SCPolice; }
+distributionTable["SC_CrownVictoriaStatePolice"] = { Normal = VehicleDistributions.SCPolicePatrol; }
+distributionTable["SC_CrownVictoriaLVPD"] = { Normal = VehicleDistributions.SCPolicePatrol; }
+distributionTable["SC_CrownVictoriaParkRanger"] = { Normal = VehicleDistributions.SCPolicePatrol; }
+distributionTable["SC_StepVanP30Police"] = { Normal = VehicleDistributions.SCPoliceTacticalBlue; }
+distributionTable["SC_StepVanP30ParkRanger"] = { Normal = VehicleDistributions.SCPoliceTacticalGreen; }
 
 distributionTable["SC_K5BlazerCiv"] = { Normal = VehicleDistributions.SCCivilian; }
 distributionTable["SC_CapriceCiv"] = { Normal = VehicleDistributions.SCCivilian; }
@@ -245,8 +339,6 @@ distributionTable["SC_G30Windowless"] = { Normal = VehicleDistributions.SCCivili
 distributionTable["SC_G30VanNormal"] = { Normal = VehicleDistributions.SCCivilian; }
 distributionTable["SC_CamaroTTop"] = { Normal = VehicleDistributions.SCCivilian; }
 distributionTable["SC_Mercedes280"] = { Normal = VehicleDistributions.SCCivilian; }
-
-distributionTable["SC_G30Ambulance"] = { Normal = VehicleDistributions.SCAmbulance; }
 
 distributionTable["SC_M1009MilitaryPolice"] = { Normal = VehicleDistributions.SCMilitaryPolice; }
 distributionTable["SC_M1009"] = { Normal = VehicleDistributions.SCMilitary; }
