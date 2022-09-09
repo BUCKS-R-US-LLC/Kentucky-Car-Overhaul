ee089c2e47a2c72f4bd0d489ce98059cd91004e6
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2022-09-08 20:52:48 -0400

    Update SCKCOVer.lua

--- Contents/mods/SCKCO - Core/media/lua/server/SCKCOVer.lua
+++ Contents/mods/SCKCO - Core/media/lua/server/SCKCOVer.lua
@@ -1,1 +1,1 @@
-Events.OnGameBoot.Add(print("Shark and Cytt's Kentucky Car Overhaul - Tweaks and Adjustments - AUG30"))
+Events.OnGameBoot.Add(print("Shark and Cytt's Kentucky Car Overhaul - M35A1, M1010, and M1008 Troop Carrier - SEPT9"))
