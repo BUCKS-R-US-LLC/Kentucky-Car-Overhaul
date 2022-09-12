a4489df18404f3dc75d814691e96004862c7dd89
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2022-09-12 18:14:03 -0400

    M35 progress

---
+++ Contents/mods/SCKCO - Core/media/lua/client/Vehicles/ISUI/M35CarMechanicsOverlay.lua
@@ -0,0 +1,37 @@
+ISCarMechanicsOverlay.CarList["Base.SC_M35A1"] = {imgPrefix = "M35A1_", x=0,y=0};
+
+ISCarMechanicsOverlay.PartList["Battery"].vehicles = ISCarMechanicsOverlay.PartList["Battery"].vehicles or {};
+ISCarMechanicsOverlay.PartList["Battery"].vehicles["caprice_"] = {img="battery", x=0,y=0,x2=0,y2=0};
+--
+ISCarMechanicsOverlay.PartList["SuspensionFrontLeft"].vehicles = ISCarMechanicsOverlay.PartList["SuspensionFrontLeft"].vehicles or {};
+ISCarMechanicsOverlay.PartList["SuspensionFrontLeft"].vehicles["caprice_"] = {img="suspension_front_left", x=0,y=0,x2=0,y2=0};
+ISCarMechanicsOverlay.PartList["SuspensionFrontRight"].vehicles = ISCarMechanicsOverlay.PartList["SuspensionFrontRight"].vehicles or {};
+ISCarMechanicsOverlay.PartList["SuspensionFrontRight"].vehicles["caprice_"] = {img="suspension_front_right", x=0,y=72,x2=0,y2=0};
+ISCarMechanicsOverlay.PartList["SuspensionRearLeft"].vehicles = ISCarMechanicsOverlay.PartList["SuspensionRearLeft"].vehicles or {};
+ISCarMechanicsOverlay.PartList["SuspensionRearLeft"].vehicles["caprice_"] = {img="suspension_rear_left", x=0,y=0,x2=0,y2=0};
+ISCarMechanicsOverlay.PartList["SuspensionRearRight"].vehicles = ISCarMechanicsOverlay.PartList["SuspensionRearRight"].vehicles or {};
+ISCarMechanicsOverlay.PartList["SuspensionRearRight"].vehicles["caprice_"] = {img="suspension_rear_right", x=0,y=0,x2=0,y2=0};
+--
+ISCarMechanicsOverlay.PartList["BrakeFrontLeft"].vehicles = ISCarMechanicsOverlay.PartList["BrakeFrontLeft"].vehicles or {};
+ISCarMechanicsOverlay.PartList["BrakeFrontLeft"].vehicles["caprice_"] = {img="brake_front_left", x=0,y=0,x2=0,y2=0};
+ISCarMechanicsOverlay.PartList["BrakeFrontRight"].vehicles = ISCarMechanicsOverlay.PartList["BrakeFrontRight"].vehicles or {};
+ISCarMechanicsOverlay.PartList["BrakeFrontRight"].vehicles["caprice_"] = {img="brake_front_right", x=0,y=0,x2=0,y2=0};
+ISCarMechanicsOverlay.PartList["BrakeRearLeft"].vehicles = ISCarMechanicsOverlay.PartList["BrakeRearLeft"].vehicles or {};
+ISCarMechanicsOverlay.PartList["BrakeRearLeft"].vehicles["caprice_"] = {img="brake_rear_left", x=0,y=0,x2=0,y2=0};
+ISCarMechanicsOverlay.PartList["BrakeRearRight"].vehicles = ISCarMechanicsOverlay.PartList["BrakeRearRight"].vehicles or {};
+ISCarMechanicsOverlay.PartList["BrakeRearRight"].vehicles["caprice_"] = {img="brake_rear_right", x=0,y=0,x2=0,y2=0};
+--
+ISCarMechanicsOverlay.PartList["DoorFrontLeft"].vehicles = ISCarMechanicsOverlay.PartList["DoorFrontLeft"].vehicles or {};
+ISCarMechanicsOverlay.PartList["DoorFrontLeft"].vehicles["caprice_"] = {x=0,y=0,x2=0,y2=0};
+ISCarMechanicsOverlay.PartList["DoorFrontRight"].vehicles = ISCarMechanicsOverlay.PartList["DoorFrontRight"].vehicles or {};
+ISCarMechanicsOverlay.PartList["DoorFrontRight"].vehicles["caprice_"] = {x=0,y=0,x2=0,y2=0};
+--
+ISCarMechanicsOverlay.PartList["DoorRear"] = {img="trunk", vehicles = {"caprice_"}};
+ISCarMechanicsOverlay.PartList["DoorRear"].vehicles = ISCarMechanicsOverlay.PartList["DoorRear"].vehicles or {};
+ISCarMechanicsOverlay.PartList["DoorRear"].vehicles["caprice_"] = {x=0,y=0,x2=0,y2=0};
+--
+ISCarMechanicsOverlay.PartList["Engine"].vehicles = ISCarMechanicsOverlay.PartList["Engine"].vehicles or {};
+ISCarMechanicsOverlay.PartList["Engine"].vehicles["caprice_"] = {x=0,y=0,x2=0,y2=0};
+--
+ISCarMechanicsOverlay.PartList["Muffler"].vehicles = ISCarMechanicsOverlay.PartList["Muffler"].vehicles or {};
+ISCarMechanicsOverlay.PartList["Muffler"].vehicles["caprice_"] = {x=0,y=0,x2=00,y2=0};
