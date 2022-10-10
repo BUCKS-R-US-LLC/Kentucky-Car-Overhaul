6080193877680e4b6a1e1f5f9c499f791f2783fc
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2022-10-10 05:43:12 -0400

    Rename

--- Contents/mods/SCKCO - Zoning/media/lua/shared/KCOSpawns.lua
+++ Contents/mods/SCKCO - Zoning/media/lua/shared/KCOSpawns.lua
@@ -1,18 +1,18 @@
 if VehicleZoneDistribution then
 
-	VehicleZoneDistribution.military_light = VehicleZoneDistribution.military_light or {};
-	VehicleZoneDistribution.military_light.vehicles = VehicleZoneDistribution.military_light.vehicles or {};
+	VehicleZoneDistribution.military_general = VehicleZoneDistribution.military_general or {};
+	VehicleZoneDistribution.military_general.vehicles = VehicleZoneDistribution.military_general.vehicles or {};
 
-	VehicleZoneDistribution.military_light.vehicles["Base.SC_M1009"] = {index = -1, spawnChance = 60};
-	VehicleZoneDistribution.military_light.vehicles["Base.SC_M1008"] = {index = -1, spawnChance = 70};
-	VehicleZoneDistribution.military_light.vehicles["Base.SC_M1008TroopCarrier"] = {index = -1, spawnChance = 70};
-	VehicleZoneDistribution.military_light.vehicles["Base.SC_M1010Ambulance"] = {index = -1, spawnChance = 50};
-	VehicleZoneDistribution.military_light.vehicles["Base.SC_M35A1"] = {index = -1, spawnChance = 60};
+	VehicleZoneDistribution.military_general.vehicles["Base.SC_M1009"] = {index = -1, spawnChance = 60};
+	VehicleZoneDistribution.military_general.vehicles["Base.SC_M1008"] = {index = -1, spawnChance = 70};
+	VehicleZoneDistribution.military_general.vehicles["Base.SC_M1008TroopCarrier"] = {index = -1, spawnChance = 70};
+	VehicleZoneDistribution.military_general.vehicles["Base.SC_M1010Ambulance"] = {index = -1, spawnChance = 50};
+	VehicleZoneDistribution.military_general.vehicles["Base.SC_M35A1"] = {index = -1, spawnChance = 60};
 
-	VehicleZoneDistribution.military_light.baseVehicleQuality = 1.1;
-	VehicleZoneDistribution.military_light.chanceToSpawnKey = 80;
-	VehicleZoneDistribution.military_light.chanceToSpawnSpecial = 0;
-	VehicleZoneDistribution.military_light.spawnRate = 100;
+	VehicleZoneDistribution.military_general.baseVehicleQuality = 1.1;
+	VehicleZoneDistribution.military_general.chanceToSpawnKey = 80;
+	VehicleZoneDistribution.military_general.chanceToSpawnSpecial = 0;
+	VehicleZoneDistribution.military_general.spawnRate = 100;
 
 	VehicleZoneDistribution.military_police = VehicleZoneDistribution.military_police or {};
 	VehicleZoneDistribution.military_police.vehicles = VehicleZoneDistribution.military_police.vehicles or {};
