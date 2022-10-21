28ec68878017ae07d86861e66da082abfa34ae40
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2022-10-21 18:29:32 -0400

    Adjustments to armor

--- Contents/mods/SCKCO - Core/media/lua/shared/Armor/CherokeeArmor.lua
+++ Contents/mods/SCKCO - Core/media/lua/shared/Armor/CherokeeArmor.lua
@@ -19,24 +19,6 @@ local part = vehicle:getPartById("Windshield")
 
 end
 
-function CherokeeEngineDoor(player, part, elapsedMinutes)
-    local vehicle = player:getVehicle()
-    if (vehicle and string.find( vehicle:getScriptName(), "Cherokee" )) then
-
-local part = vehicle:getPartById("EngineDoor")
-        if (vehicle:getPartById("JeepCherokeeBullbar"):getCondition() > 1) and (vehicle:getPartById("EngineDoor"):getCondition() < 70) and (vehicle:getPartById("JeepCherokeeBullbar"):getInventoryItem()) then
-
-            sendClientCommand(player, "vehicle", "setPartCondition", { vehicle = vehicle:getId(), part = part:getId(), condition = 70 })
-            vehicle:getPartById("JeepCherokeeBullbar"):setCondition(vehicle:getPartById("JeepCherokeeBullbar"):getCondition()-1)
-
-        end
-        vehicle:transmitPartModData(EngineDoor)
-    end
-
-
-end
-
-
 function CherokeeGasTank(player, part, elapsedMinutes)
     local vehicle = player:getVehicle()
     if (vehicle and string.find( vehicle:getScriptName(), "Cherokee" )) then
@@ -58,10 +40,10 @@ function CherokeeWindowFrontLeft(player, part, elapsedMinutes)
     if (vehicle and string.find( vehicle:getScriptName(), "Cherokee" )) then
 
         local part = vehicle:getPartById("WindowFrontLeft")
-        if (vehicle:getPartById("JeepCherokeeWindowArmor"):getCondition() > 1) and (vehicle:getPartById("WindowFrontLeft"):getCondition() < 70) and (vehicle:getPartById("JeepCherokeeWindowArmor"):getInventoryItem()) then
+        if (vehicle:getPartById("JeepCherokeeFrontLeftWindowArmor"):getCondition() > 1) and (vehicle:getPartById("WindowFrontLeft"):getCondition() < 70) and (vehicle:getPartById("JeepCherokeeFrontLeftWindowArmor"):getInventoryItem()) then
 
             sendClientCommand(player, "vehicle", "setPartCondition", { vehicle = vehicle:getId(), part = part:getId(), condition = 70 })
-            vehicle:getPartById("JeepCherokeeWindowArmor"):setCondition(vehicle:getPartById("JeepCherokeeWindowArmor"):getCondition()-1)
+            vehicle:getPartById("JeepCherokeeFrontLeftWindowArmor"):setCondition(vehicle:getPartById("JeepCherokeeFrontLeftWindowArmor"):getCondition()-1)
 
         end
         vehicle:transmitPartModData(WindowFrontLeft)
@@ -75,11 +57,47 @@ function CherokeeWindowFrontRight(player, part, elapsedMinutes)
     if (vehicle and string.find( vehicle:getScriptName(), "Cherokee" )) then
 
         local part = vehicle:getPartById("WindowFrontRight")
-        if (vehicle:getPartById("JeepCherokeeWindowArmor"):getCondition() > 1) and (vehicle:getPartById("WindowFrontRight"):getCondition() < 70) and (vehicle:getPartById("JeepCherokeeWindowArmor"):getInventoryItem()) then
+        if (vehicle:getPartById("JeepCherokeeFrontRightWindowArmor"):getCondition() > 1) and (vehicle:getPartById("WindowFrontRight"):getCondition() < 70) and (vehicle:getPartById("JeepCherokeeFrontRightWindowArmor"):getInventoryItem()) then
 
 
             sendClientCommand(player, "vehicle", "setPartCondition", { vehicle = vehicle:getId(), part = part:getId(), condition = 70 })
-            vehicle:getPartById("JeepCherokeeWindowArmor"):setCondition(vehicle:getPartById("JeepCherokeetWindowArmor"):getCondition()-1)
+            vehicle:getPartById("JeepCherokeeFrontRightWindowArmor"):setCondition(vehicle:getPartById("JeepCherokeeFrontRightWindowArmor"):getCondition()-1)
+
+        end
+        vehicle:transmitPartModData(WindowFrontRight)
+    end
+
+
+end
+
+
+function CherokeeWindowRearLeft(player, part, elapsedMinutes)
+    local vehicle = player:getVehicle()
+    if (vehicle and string.find( vehicle:getScriptName(), "Cherokee" )) then
+
+        local part = vehicle:getPartById("WindowRearLeft")
+        if (vehicle:getPartById("JeepCherokeeRearLeftWindowArmor"):getCondition() > 1) and (vehicle:getPartById("WindowRearLeft"):getCondition() < 70) and (vehicle:getPartById("JeepCherokeeRearLeftWindowArmor"):getInventoryItem()) then
+
+            sendClientCommand(player, "vehicle", "setPartCondition", { vehicle = vehicle:getId(), part = part:getId(), condition = 70 })
+            vehicle:getPartById("JeepCherokeeRearLeftWindowArmor"):setCondition(vehicle:getPartById("JeepCherokeeRearLeftWindowArmor"):getCondition()-1)
+
+        end
+        vehicle:transmitPartModData(WindowFrontLeft)
+    end
+
+
+end
+
+function CherokeeWindowRearRight(player, part, elapsedMinutes)
+    local vehicle = player:getVehicle()
+    if (vehicle and string.find( vehicle:getScriptName(), "Cherokee" )) then
+
+        local part = vehicle:getPartById("WindowRearRight")
+        if (vehicle:getPartById("JeepCherokeeRearRightWindowArmor"):getCondition() > 1) and (vehicle:getPartById("WindowRearRight"):getCondition() < 70) and (vehicle:getPartById("JeepCherokeeRearRightWindowArmor"):getInventoryItem()) then
+
+
+            sendClientCommand(player, "vehicle", "setPartCondition", { vehicle = vehicle:getId(), part = part:getId(), condition = 70 })
+            vehicle:getPartById("JeepCherokeeRearRightWindowArmor"):setCondition(vehicle:getPartById("JeepCherokeeRearRightWindowArmor"):getCondition()-1)
 
         end
         vehicle:transmitPartModData(WindowFrontRight)
@@ -92,14 +110,123 @@ function CherokeeBullbar(player, part, elapsedMinutes)
     local vehicle = player:getVehicle()
     if (vehicle and string.find( vehicle:getScriptName(), "Cherokee" )) then
 
-local part = vehicle:getPartById("EngineDoor")
-        if (vehicle:getPartById("JeepCherokeeBullbar"):getCondition() > 1) and (vehicle:getPartById("EngineDoor"):getCondition() < 70) and (vehicle:getPartById("JeepCherokeeBullbar"):getInventoryItem()) then
+local part = vehicle:getPartById("EngineWindow")
+        if (vehicle:getPartById("JeepCherokeeBullbar"):getCondition() > 1) and (vehicle:getPartById("EngineWindow"):getCondition() < 70) and (vehicle:getPartById("JeepCherokeeBullbar"):getInventoryItem()) then
 
             sendClientCommand(player, "vehicle", "setPartCondition", { vehicle = vehicle:getId(), part = part:getId(), condition = 100 })
-            vehicle:getPartById("JeepCherokeeBullbar"):setCondition(vehicle:getPartById("CherokeeBumper"):getCondition()-1)
+            vehicle:getPartById("JeepCherokeeBullbar"):setCondition(vehicle:getPartById("JeepCherokeeBullbar"):getCondition()-1)
 
         end
-        vehicle:transmitPartModData(EngineDoor)
+        vehicle:transmitPartModData(EngineWindow)
+    end
+
+
+end
+
+
+function CherokeeDoorFrontLeft(player, part, elapsedMinutes)
+    local vehicle = player:getVehicle()
+    if (vehicle and string.find( vehicle:getScriptName(), "Cherokee" )) then
+
+        local part = vehicle:getPartById("DoorFrontLeft")
+        if (vehicle:getPartById("JeepCherokeeFrontLeftDoorArmor"):getCondition() > 1) and (vehicle:getPartById("DoorFrontLeft"):getCondition() < 70) and (vehicle:getPartById("JeepCherokeeFrontLeftDoorArmor"):getInventoryItem()) then
+
+            sendClientCommand(player, "vehicle", "setPartCondition", { vehicle = vehicle:getId(), part = part:getId(), condition = 70 })
+            vehicle:getPartById("JeepCherokeeFrontLeftDoorArmor"):setCondition(vehicle:getPartById("JeepCherokeeFrontLeftDoorArmor"):getCondition()-1)
+
+        end
+        vehicle:transmitPartModData(DoorFrontLeft)
+    end
+
+
+end
+
+
+function CherokeeDoorFrontRight(player, part, elapsedMinutes)
+    local vehicle = player:getVehicle()
+    if (vehicle and string.find( vehicle:getScriptName(), "Cherokee" )) then
+
+        local part = vehicle:getPartById("DoorFrontRight")
+        if (vehicle:getPartById("JeepCherokeeFrontRightDoorArmor"):getCondition() > 1) and (vehicle:getPartById("DoorFrontRight"):getCondition() < 70) and (vehicle:getPartById("JeepCherokeeFrontRightDoorArmor"):getInventoryItem()) then
+
+
+            sendClientCommand(player, "vehicle", "setPartCondition", { vehicle = vehicle:getId(), part = part:getId(), condition = 70 })
+            vehicle:getPartById("JeepCherokeeFrontRightDoorArmor"):setCondition(vehicle:getPartById("JeepCherokeeFrontRightWindowArmor"):getCondition()-1)
+
+        end
+        vehicle:transmitPartModData(DoorFrontRight)
+    end
+
+
+end
+
+
+function CherokeeDoorRearLeft(player, part, elapsedMinutes)
+    local vehicle = player:getVehicle()
+    if (vehicle and string.find( vehicle:getScriptName(), "Cherokee" )) then
+
+        local part = vehicle:getPartById("DoorRearLeft")
+        if (vehicle:getPartById("JeepCherokeeRearLeftDoorArmor"):getCondition() > 1) and (vehicle:getPartById("DoorRearLeft"):getCondition() < 70) and (vehicle:getPartById("JeepCherokeeRearLeftDoorArmor"):getInventoryItem()) then
+
+            sendClientCommand(player, "vehicle", "setPartCondition", { vehicle = vehicle:getId(), part = part:getId(), condition = 70 })
+            vehicle:getPartById("JeepCherokeeRearLeftDoorArmor"):setCondition(vehicle:getPartById("JeepCherokeeRearLeftDoorArmor"):getCondition()-1)
+
+        end
+        vehicle:transmitPartModData(DoorRearLeft)
+    end
+
+
+end
+
+function CherokeeDoorRearRight(player, part, elapsedMinutes)
+    local vehicle = player:getVehicle()
+    if (vehicle and string.find( vehicle:getScriptName(), "Cherokee" )) then
+
+        local part = vehicle:getPartById("DoorRearRight")
+        if (vehicle:getPartById("JeepCherokeeRearRightDoorArmor"):getCondition() > 1) and (vehicle:getPartById("DoorRearRight"):getCondition() < 70) and (vehicle:getPartById("JeepCherokeeRearRightDoorArmor"):getInventoryItem()) then
+
+
+            sendClientCommand(player, "vehicle", "setPartCondition", { vehicle = vehicle:getId(), part = part:getId(), condition = 70 })
+            vehicle:getPartById("JeepCherokeeRearRightDoorArmor"):setCondition(vehicle:getPartById("JeepCherokeeRearRightWindowArmor"):getCondition()-1)
+
+        end
+        vehicle:transmitPartModData(DoorRearRight)
+    end
+
+
+end
+
+
+function CherokeeWindowMiddleLeft(player, part, elapsedMinutes)
+    local vehicle = player:getVehicle()
+    if (vehicle and string.find( vehicle:getScriptName(), "Cherokee" )) then
+
+        local part = vehicle:getPartById("WindowMiddleLeft")
+        if (vehicle:getPartById("JeepCherokeeMiddleLeftWindowArmor"):getCondition() > 1) and (vehicle:getPartById("WindowMiddleLeft"):getCondition() < 70) and (vehicle:getPartById("JeepCherokeeMiddleLeftWindowArmor"):getInventoryItem()) then
+
+            sendClientCommand(player, "vehicle", "setPartCondition", { vehicle = vehicle:getId(), part = part:getId(), condition = 70 })
+            vehicle:getPartById("JeepCherokeeMiddleLeftWindowArmor"):setCondition(vehicle:getPartById("JeepCherokeeMiddleLeftWindowArmor"):getCondition()-1)
+
+        end
+        vehicle:transmitPartModData(WindowMiddleLeft)
+    end
+
+
+end
+
+function CherokeeWindowRearRight(player, part, elapsedMinutes)
+    local vehicle = player:getVehicle()
+    if (vehicle and string.find( vehicle:getScriptName(), "Cherokee" )) then
+
+        local part = vehicle:getPartById("WindowMiddleRight")
+        if (vehicle:getPartById("JeepCherokeeMiddleRightWindowArmor"):getCondition() > 1) and (vehicle:getPartById("WindowMiddleRight"):getCondition() < 70) and (vehicle:getPartById("JeepCherokeeMiddleRightWindowArmor"):getInventoryItem()) then
+
+
+            sendClientCommand(player, "vehicle", "setPartCondition", { vehicle = vehicle:getId(), part = part:getId(), condition = 70 })
+            vehicle:getPartById("JeepCherokeeMiddleRightWindowArmor"):setCondition(vehicle:getPartById("JeepCherokeeRearRightWindowArmor"):getCondition()-1)
+
+        end
+        vehicle:transmitPartModData(WindowMiddleRight)
     end
 
 
@@ -107,9 +234,14 @@ end
 
 Events.OnPlayerUpdate.Add(CherokeeWindshield);
 Events.OnPlayerUpdate.Add(CherokeeGasTank);
-Events.OnPlayerUpdate.Add(CherokeeEngineDoor);
+Events.OnPlayerUpdate.Add(CherokeeEngineWindow);
 Events.OnPlayerUpdate.Add(CherokeeDoorFrontLeft);
 Events.OnPlayerUpdate.Add(CherokeeDoorFrontRight);
+Events.OnPlayerUpdate.Add(CherokeeDoorRearLeft);
+Events.OnPlayerUpdate.Add(CherokeeDoorRearRight);
 Events.OnPlayerUpdate.Add(CherokeeWindowFrontLeft);
 Events.OnPlayerUpdate.Add(CherokeeWindowFrontRight);
-Events.OnPlayerUpdate.Add(CherokeeBullbar);
+Events.OnPlayerUpdate.Add(CherokeeWindowMiddleLeft);
+Events.OnPlayerUpdate.Add(CherokeeWindowMiddleRight);
+Events.OnPlayerUpdate.Add(CherokeeWindowRearLeft);
+Events.OnPlayerUpdate.Add(CherokeeWindowRearRight);
