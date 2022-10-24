2509f043774805aba6c4217f742b9ebe23e67e52
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2022-10-24 16:45:15 -0400

    Update SCKCO_SpawnList.lua

--- Contents/mods/SCKCO - Core/media/lua/server/Vehicles/SCKCO_SpawnList.lua
+++ Contents/mods/SCKCO - Core/media/lua/server/Vehicles/SCKCO_SpawnList.lua
@@ -2,14 +2,14 @@ if VehicleZoneDistribution then
 
     VehicleZoneDistribution.medium.vehicles["Base.SC_CapriceCiv"] = {index = -1, spawnChance = 15};
     VehicleZoneDistribution.medium.vehicles["Base.SC_JeepCherokee"] = {index = -1, spawnChance = 15};
-    VehicleZoneDistribution.medium.vehicles["Base.SC_MazdaB"] = {index = -1, spawnChance = 15};
+    VehicleZoneDistribution.medium.vehicles["Base.SC_MazdaB"] = {index = -1, spawnChance = 40};
     VehicleZoneDistribution.medium.vehicles["Base.SC_PlymouthVoyager"] = {index = -1, spawnChance = 30};
     VehicleZoneDistribution.medium.spawnRate = 30;
     VehicleZoneDistribution.medium.chanceToPartDamage = 10;
     
     VehicleZoneDistribution.good.vehicles["Base.SC_CapriceCiv"] = {index = -1, spawnChance = 15};
     VehicleZoneDistribution.good.vehicles["Base.SC_JeepCherokee"] = {index = -1, spawnChance = 15};
-    VehicleZoneDistribution.good.vehicles["Base.SC_MazdaB"] = {index = -1, spawnChance = 30};
+    VehicleZoneDistribution.good.vehicles["Base.SC_MazdaB"] = {index = -1, spawnChance = 35};
     VehicleZoneDistribution.good.vehicles["Base.SC_PlymouthVoyager"] = {index = -1, spawnChance = 30};
     VehicleZoneDistribution.good.vehicles["Base.SC_CadillacFuneralCoach"] = {index = -1, spawnChance = 1};
     VehicleZoneDistribution.good.spawnRate = 30;
