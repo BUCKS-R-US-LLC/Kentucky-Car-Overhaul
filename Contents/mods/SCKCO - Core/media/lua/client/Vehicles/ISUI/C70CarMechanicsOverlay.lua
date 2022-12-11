62233420c59c51afe97c7aa2eeb0f9c5ba2561d0
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2022-12-11 13:23:32 -0500

    Assigned mech UI

--- Contents/mods/SCKCO - Core/media/lua/client/Vehicles/ISUI/C70CarMechanicsOverlay.lua
+++ Contents/mods/SCKCO - Core/media/lua/client/Vehicles/ISUI/C70CarMechanicsOverlay.lua
@@ -1,16 +1,6 @@
-ISCarMechanicsOverlay.CarList["Base.SC_FordExplorer"] = {imgPrefix = "explorer_", x=0,y=0};
-ISCarMechanicsOverlay.CarList["Base.SC_FordBronco"] = {imgPrefix = "bronco1992_", x=0,y=0};
-ISCarMechanicsOverlay.CarList["Base.SC_FordF150"] = {imgPrefix = "f150_", x=0,y=0};
-ISCarMechanicsOverlay.CarList["Base.SC_FordF700BoxTruck"] = {imgPrefix = "f700box_", x=0,y=0};
-ISCarMechanicsOverlay.CarList["Base.SC_FordF700DumpTruck"] = {imgPrefix = "f700dump_", x=0,y=0};
-ISCarMechanicsOverlay.CarList["Base.SC_FordF700PanelTruck"] = {imgPrefix = "f700panel_", x=0,y=0};
-ISCarMechanicsOverlay.CarList["Base.SC_FordF700ArmoredPolice"] = {imgPrefix = "f700dump_", x=0,y=0};
-ISCarMechanicsOverlay.CarList["Base.SC_FordF700ArmoredBank"] = {imgPrefix = "f700dump_", x=0,y=0};
-ISCarMechanicsOverlay.CarList["Base.SC_FordF700ArmoredBank"] = {imgPrefix = "f700dump_", x=0,y=0};
-ISCarMechanicsOverlay.CarList["Base.SC_FordF700ArmoredBank"] = {imgPrefix = "f700dump_", x=0,y=0};
-ISCarMechanicsOverlay.CarList["Base.SUV"] = {imgPrefix = "explorer_", x=0,y=0};
-ISCarMechanicsOverlay.CarList["Base.ModernCar"] = {imgPrefix = "taurus86_", x=0,y=0};
---
+ISCarMechanicsOverlay.CarList["Base.SC_ChevroletC70Survivalist"] = {imgPrefix = "C70survivor_", x=0,y=0};
+ISCarMechanicsOverlay.CarList["Base.SC_ChevroletC70GrainTruck"] = {imgPrefix = "c70agri_", x=0,y=0};
+
 ISCarMechanicsOverlay.PartList["Battery"].vehicles = ISCarMechanicsOverlay.PartList["Battery"].vehicles or {};
 ISCarMechanicsOverlay.PartList["Battery"].vehicles["caprice_"] = {img="battery", x=0,y=0,x2=0,y2=0};
 --
