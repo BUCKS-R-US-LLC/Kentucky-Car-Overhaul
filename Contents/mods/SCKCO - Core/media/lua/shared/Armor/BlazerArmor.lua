5a5a7c5beb76a79919e74f6572d9edc8916af43a
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2022-12-23 20:24:18 -0500

    Blazer/CUCV have armor for bullbars now

---
+++ Contents/mods/SCKCO - Core/media/lua/shared/Armor/BlazerArmor.lua
@@ -0,0 +1,40 @@
+---
+--- Created by cytt0rak
+---  WIP
+
+function K5Bullbar(player, part, elapsedMinutes)
+    local vehicle = player:getVehicle()
+    if (vehicle and string.find( vehicle:getScriptName(), "K5" )) then
+
+local part = vehicle:getPartById("EngineDoor")
+        if (vehicle:getPartById("K5Bullbar"):getCondition() > 1) and (vehicle:getPartById("EngineDoor"):getCondition() < 70) and (vehicle:getPartById("K5Bullbar"):getInventoryItem()) then
+
+            sendClientCommand(player, "vehicle", "setPartCondition", { vehicle = vehicle:getId(), part = part:getId(), condition = 100 })
+            vehicle:getPartById("K5Bullbar"):setCondition(vehicle:getPartById("K5Bullbar"):getCondition()-1)
+
+        end
+        vehicle:transmitPartModData(EngineDoor)
+    end
+
+
+end
+
+function CUCVBullbar(player, part, elapsedMinutes)
+    local vehicle = player:getVehicle()
+    if (vehicle and string.find( vehicle:getScriptName(), "M10" )) then
+
+local part = vehicle:getPartById("EngineDoor")
+        if (vehicle:getPartById("K5Bullbar"):getCondition() > 1) and (vehicle:getPartById("EngineDoor"):getCondition() < 70) and (vehicle:getPartById("K5Bullbar"):getInventoryItem()) then
+
+            sendClientCommand(player, "vehicle", "setPartCondition", { vehicle = vehicle:getId(), part = part:getId(), condition = 100 })
+            vehicle:getPartById("K5Bullbar"):setCondition(vehicle:getPartById("K5Bullbar"):getCondition()-1)
+
+        end
+        vehicle:transmitPartModData(EngineDoor)
+    end
+
+
+end
+
+Events.OnPlayerUpdate.Add(K5Bullbar);
+Events.OnPlayerUpdate.Add(CUCVBullbar);
