c275ccc8f0a03a30172b7192e6713e422045fc46
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2023-03-11 13:46:08 -0500

    Grumman LLV progress

--- Contents/mods/SCKCO - Core/media/lua/client/Vehicles/ISUI/GrummanCarMechanicsOverlay.lua
+++ Contents/mods/SCKCO - Core/media/lua/client/Vehicles/ISUI/GrummanCarMechanicsOverlay.lua
@@ -1,4 +1,4 @@
-ISCarMechanicsOverlay.CarList["Base.SC_GrummanLLV"] = {imgPrefix = "rabbit_", x=0,y=0};
+ISCarMechanicsOverlay.CarList["Base.SC_GrummanLLV"] = {imgPrefix = "grum_", x=0,y=0};
 --
 ISCarMechanicsOverlay.PartList["Battery"].vehicles = ISCarMechanicsOverlay.PartList["Battery"].vehicles or {};
 ISCarMechanicsOverlay.PartList["Battery"].vehicles["caprice_"] = {img="battery", x=0,y=0,x2=0,y2=0};
