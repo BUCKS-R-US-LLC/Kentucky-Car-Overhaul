1438c9b528bbc5c6537198dfbdc12f9cb45dce2b
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2023-07-18 19:10:45 -0400

    Added more loot spawns to vehicles and fixed older ones

--- Contents/mods/SCKCO - Core/media/lua/server/Vehicles/SCKCO_VehicleDistro.lua
+++ Contents/mods/SCKCO - Core/media/lua/server/Vehicles/SCKCO_VehicleDistro.lua
@@ -9,10 +9,10 @@ VehicleDistributions.SCMilitary = {
             "HandTorch", 1,
             "Radio.WalkieTalkie4", 10,
 
-            -- SMUI --
+            -- SPGO. --
 
-            "SMUIClothing.Hat_PatrolCap", 10,
-            "SMUIClothing.Hat.MilitaryHelmet", 10,
+            "SPGOClothing.Hat_PatrolCapWoodland", 10,
+            "SPGOClothing.Hat.PASGTHelmetWoodland", 10,
             "Vest_BulletArmy", 10,
 
             -- MEA -- 
@@ -42,10 +42,10 @@ VehicleDistributions.SCMilitary = {
             "Battery", 10,
             "HandTorch", 4,
 
-            -- SMUI -- 
+            -- SPGO. -- 
 
-            "SMUIClothing.Hat_WatchCap", 10,
-            "SMUIClothing.Glasses_GIGlasses", 10,
+            "SPGOClothing.Hat_WatchCap", 10,
+            "SPGOClothing.Glasses_GIGlasses", 10,
 
 
         },
@@ -76,9 +76,9 @@ VehicleDistributions.SCMilitaryPolice = {
             "HandTorch", 4,
             "Radio.WalkieTalkie4", 10,
 
-            -- SMUI --
+            -- SPGO. --
 
-            "SMUIClothing.MPBrassard", 10,
+            "SPGOClothing.MPBrassard", 10,
             "Vest_BulletArmy", 10,
         },
         junk = {
@@ -154,6 +154,31 @@ VehicleDistributions.SCCivilian = {
 	TruckBed = VehicleDistributions.SCCivilianMisc;
 }
 
+distributionTable["SC_K5BlazerFossoil"] = { Normal = VehicleDistributions.SCCivilian; }
+distributionTable["SC_K5BlazerMcCoys"] = { Normal = VehicleDistributions.SCCivilian; }
+distributionTable["SC_MazdaB"] = { Normal = VehicleDistributions.SCCivilian; }
+distributionTable["SC_PlymouthVoyager"] = { Normal = VehicleDistributions.SCCivilian; }
+distributionTable["SC_Porsche911G50"] = { Normal = VehicleDistributions.SCCivilian; }
+distributionTable["SC_BMW520i"] = { Normal = VehicleDistributions.SCCivilian; }
+distributionTable["SC_CadillacBrougham"] = { Normal = VehicleDistributions.SCCivilian; }
+distributionTable["SC_JaguarXJ6"] = { Normal = VehicleDistributions.SCCivilian; }
+distributionTable["SC_CadillacFleetwood"] = { Normal = VehicleDistributions.SCCivilian; }
+distributionTable["SC_ChevroletCavalier"] = { Normal = VehicleDistributions.SCCivilian; }
+distributionTable["SC_FordBronco"] = { Normal = VehicleDistributions.SCCivilian; }
+distributionTable["SC_FordF150"] = { Normal = VehicleDistributions.SCCivilian; }
+distributionTable["SC_FordMustang"] = { Normal = VehicleDistributions.SCCivilian; }
+distributionTable["SC_JeepCJ7"] = { Normal = VehicleDistributions.SCCivilian; }
+distributionTable["SC_JeepCherokee"] = { Normal = VehicleDistributions.SCCivilian; }
+distributionTable["SC_CrownVictoria92"] = { Normal = VehicleDistributions.SCCivilian; }
+distributionTable["SC_GeoTracker"] = { Normal = VehicleDistributions.SCCivilian; }
+distributionTable["SC_M880Bubba"] = { Normal = VehicleDistributions.SCCivilian; }
+distributionTable["SC_ChevroletC70GrainTruck"] = { Normal = VehicleDistributions.SCCivilian; }
+distributionTable["SC_FordF700PanelTruck"] = { Normal = VehicleDistributions.SCCivilian; }
+distributionTable["SC_FordF700DumpTruck"] = { Normal = VehicleDistributions.SCCivilian; }
+distributionTable["SC_FordF700BoxTruck"] = { Normal = VehicleDistributions.SCCivilian; }
+distributionTable["SC_FordF700Greenes"] = { Normal = VehicleDistributions.SCCivilian; }
+distributionTable["SC_FordF700GigaMart"] = { Normal = VehicleDistributions.SCCivilian; }
+distributionTable["SC_FordF700MacTools"] = { Normal = VehicleDistributions.SCCivilian; }
 
 distributionTable["SC_Caprice9C1StatePolice"] = { Normal = VehicleDistributions.SCPolicePatrol; }
 distributionTable["SC_Caprice9C1LVPD"] = { Normal = VehicleDistributions.SCPolicePatrol; }
@@ -163,6 +188,11 @@ distributionTable["SC_FordMustangStatePolice"] = { Normal = VehicleDistributions
 distributionTable["SC_FordMustangJeffersonCounty"] = { Normal = VehicleDistributions.SCPolicePatrol; }
 distributionTable["SC_FordMustangUndercover"] = { Normal = VehicleDistributions.SCPolicePatrol; }
 distributionTable["SC_CrownVictoriaLVPD"] = { Normal = VehicleDistributions.SCPolicePatrol; }
+distributionTable["SC_CrownVictoria92LVPD"] = { Normal = VehicleDistributions.SCPolicePatrol; }
+distributionTable["SC_CrownVictoria92JeffersonPD"] = { Normal = VehicleDistributions.SCPolicePatrol; }
+distributionTable["SC_CrownVictoria92WarrenPD"] = { Normal = VehicleDistributions.SCPolicePatrol; }
+distributionTable["SC_CrownVictoria92MuhlenbergPD"] = { Normal = VehicleDistributions.SCPolicePatrol; }
+distributionTable["SC_CrownVictoria92StatePolice"] = { Normal = VehicleDistributions.SCPolicePatrol; }
 
 distributionTable["SC_CapriceCiv"] = { Normal = VehicleDistributions.SCCivilian; }
 distributionTable["SC_StepVanP30Civ"] = { Normal = VehicleDistributions.SCCivilian; }
@@ -173,6 +203,12 @@ distributionTable["SC_CamaroTTop"] = { Normal = VehicleDistributions.SCCivilian;
 distributionTable["SC_Mercedes280"] = { Normal = VehicleDistributions.SCCivilian; }
 
 distributionTable["SC_M1009MilitaryPolice"] = { Normal = VehicleDistributions.SCMilitaryPolice; }
+distributionTable["SC_CadillacGageRanger"] = { Normal = VehicleDistributions.SCMilitaryPolice; }
 distributionTable["SC_CadillacGageRangerLights"] = { Normal = VehicleDistributions.SCMilitaryPolice; }
 distributionTable["SC_M1009"] = { Normal = VehicleDistributions.SCMilitary; }
 distributionTable["SC_M1008"] = { Normal = VehicleDistributions.SCMilitary; }
+distributionTable["SC_M1028A3"] = { Normal = VehicleDistributions.SCMilitary; }
+distributionTable["SC_M1025"] = { Normal = VehicleDistributions.SCMilitary; }
+distributionTable["SC_M998"] = { Normal = VehicleDistributions.SCMilitary; }
+distributionTable["SC_M151A2"] = { Normal = VehicleDistributions.SCMilitary; }
+distributionTable["SC_M151A2Cleaners"] = { Normal = VehicleDistributions.SCMilitary; }
