8728515b4a91559e51cda8e7088ca2cd3c5ab792
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2023-07-22 23:24:11 -0400

    Update SC_Animations.lua

--- Contents/mods/SCKCO - Core/media/lua/client/SC_Animations.lua
+++ Contents/mods/SCKCO - Core/media/lua/client/SC_Animations.lua
@@ -212,7 +212,13 @@ local vehicleToSeatAnimations = {
 		["seat3"]="Bob_SitGround_Idle",
 	},
 
-	
+	["Base.SC_BMW520i"] = {
+        ["seat0"]="Bob_Idle_Driver",
+		["seat1"]="Shark_Idle_Passenger",
+		["seat2"]="Shark_Idle_Passenger",
+		["seat3"]="Shark_Idle_Passenger",
+	},
+
 	["Base.SC_M151A2"] = {
         ["seat0"]="Bob_Idle_Driver",
 		["seat1"]="Shark_Idle_Passenger",
