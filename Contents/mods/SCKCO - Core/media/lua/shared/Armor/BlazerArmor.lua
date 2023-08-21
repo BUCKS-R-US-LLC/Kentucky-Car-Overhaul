8a4e6ff2d771b9c125c179924505881a64ab8f4c
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2023-08-21 05:18:33 -0400

    Update BlazerArmor.lua

--- Contents/mods/SCKCO - Core/media/lua/shared/Armor/BlazerArmor.lua
+++ Contents/mods/SCKCO - Core/media/lua/shared/Armor/BlazerArmor.lua
@@ -2,15 +2,48 @@
 --- Created by cytt0rak
 ---  WIP
 
+function BlazerEngine(player, part, elapsedMinutes)
+    local vehicle = player:getVehicle()
+    if (vehicle and string.find( vehicle:getScriptName(), "SC_K5BlazerFossoil", "SC_K5BlazerMcCoys", "PickUpVan", "PickUpVanLights", "PickUpVanLightsFire", "PickUpVanLightsPolice", "PickupVanLightsMccoy")) then
+
+local part = vehicle:getPartById("Engine")
+        if (vehicle:getPartById("Engine"):getCondition() < 70) and (vehicle:getPartById("Engine"):getInventoryItem()) then
+
+            sendClientCommand(player, "vehicle", "setPartCondition", { vehicle = vehicle:getId(), part = part:getId(), condition = 40 })
+
+        end
+        vehicle:transmitPartModData(Engine)
+    end
+
+
+end
+
 function K5Bullbar(player, part, elapsedMinutes)
     local vehicle = player:getVehicle()
-    if (vehicle and string.find( vehicle:getScriptName(), "SC_K5BlazerFossoil", "SC_K5BlazerMcCoys", "PickUpVan", "PickUpVanLights", "PickUpVanLightsFire", "PickUpLightsPolice", "PickupVanLightsMccoy")) then
+    if (vehicle and string.find( vehicle:getScriptName(), "SC_K5BlazerFossoil", "SC_K5BlazerMcCoys", "PickUpVan", "PickUpVanLights", "PickUpVanLightsFire", "PickupVanLightsMccoy")) then
 
 local part = vehicle:getPartById("EngineDoor")
-        if (vehicle:getPartById("K5Bullbar"):getCondition() > 1) and (vehicle:getPartById("EngineDoor"):getCondition() < 70) and (vehicle:getPartById("K5Bullbar"):getInventoryItem()) then
+        if (vehicle:getPartById("K5Bullbar"):getCondition() > 1) and (vehicle:getPartById("EngineDoor"):getCondition() < 80) and (vehicle:getPartById("K5Bullbar"):getInventoryItem()) then
 
             sendClientCommand(player, "vehicle", "setPartCondition", { vehicle = vehicle:getId(), part = part:getId(), condition = 100 })
             vehicle:getPartById("K5Bullbar"):setCondition(vehicle:getPartById("K5Bullbar"):getCondition()-1)
+
+        end
+        vehicle:transmitPartModData(EngineDoor)
+    end
+
+
+end
+
+function K5BullbarPolice(player, part, elapsedMinutes)
+    local vehicle = player:getVehicle()
+    if (vehicle and string.find( vehicle:getScriptName(), "PickUpVanLightsPolice")) then
+
+local part = vehicle:getPartById("EngineDoor")
+        if (vehicle:getPartById("K5BullbarPolice"):getCondition() > 1) and (vehicle:getPartById("EngineDoor"):getCondition() < 100) and (vehicle:getPartById("K5BullbarPolice"):getInventoryItem()) then
+
+            sendClientCommand(player, "vehicle", "setPartCondition", { vehicle = vehicle:getId(), part = part:getId(), condition = 90})
+            vehicle:getPartById("K5BullbarPolice"):setCondition(vehicle:getPartById("K5BullbarPolice"):getCondition()-1)
 
         end
         vehicle:transmitPartModData(EngineDoor)
@@ -37,4 +70,5 @@ local part = vehicle:getPartById("EngineDoor")
 end
 
 Events.OnPlayerUpdate.Add(K5Bullbar);
+Events.OnPlayerUpdate.Add(K5BullbarPolice);
 Events.OnPlayerUpdate.Add(CUCVBullbar);
