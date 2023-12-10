c8e5c6347b1494ee089cd03da64e23352ae059f6
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2023-12-10 12:38:55 -0500

    Armor sorting

---
+++ Contents/mods/SCKCO - Core/media/lua/shared/Armor/Sports/PorscheArmor.lua
@@ -0,0 +1,21 @@
+---
+--- Created by cytt0rak
+---  WIP
+
+function PorscheEngine(player, part, elapsedMinutes)
+    local vehicle = player:getVehicle()
+    if (vehicle and string.find( vehicle:getScriptName(), "SC_Porsche911G50")) then
+
+local part = vehicle:getPartById("Engine")
+        if (vehicle:getPartById("Engine"):getCondition() < 20) and (vehicle:getPartById("Engine"):getInventoryItem()) then
+
+            sendClientCommand(player, "vehicle", "setPartCondition", { vehicle = vehicle:getId(), part = part:getId(), condition = 30 })
+
+        end
+        vehicle:transmitPartModData(Engine)
+    end
+
+
+end
+
+Events.OnPlayerUpdate.Add(PorscheEngine);
