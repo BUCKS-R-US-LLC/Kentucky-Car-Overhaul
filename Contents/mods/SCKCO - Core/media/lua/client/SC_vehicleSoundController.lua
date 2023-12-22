3ce77ac9232559615dee5eb3bba46358da05a9b7
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2023-12-22 18:07:07 -0500

    Update SC_vehicleSoundController.lua

--- Contents/mods/SCKCO - Core/media/lua/client/SC_vehicleSoundController.lua
+++ Contents/mods/SCKCO - Core/media/lua/client/SC_vehicleSoundController.lua
@@ -4,9 +4,9 @@ vehicleSoundController.library = {
 
     ["AirBrakeSC"] = {
         { sound = "AirBrake",
-          speedIsGreaterThan = 0,
-          speedIsLessThan = 0,
-          --stopSound = true,
+          speedIsGreaterThan = 1,
+          speedIsLessThan = -1,
+          stopSound = false,
           isEngineRunning = true,
           isBraking = true,
         },
@@ -42,16 +42,19 @@ function vehicleSoundController.handleUpdate(player)
 
                     local bSpeedLessThan = data.speedIsLessThan and vehicleSpeed < data.speedIsLessThan
                     local bSpeedGreaterThan = data.speedIsGreaterThan and vehicleSpeed > data.speedIsGreaterThan
-                    local invertedCase = data.speedIsLessThan and data.speedIsGreaterThan and data.speedIsLessThan > data.speedIsGreaterThan
+                    local invertedCase = data.speedIsLessThan and data.speedIsGreaterThan and (data.speedIsLessThan > data.speedIsGreaterThan)
 
+                    local doNotStop = data.stopSound==false
                     local notValid = (invertedCase and (not bSpeedLessThan or not bSpeedGreaterThan)) or (not bSpeedLessThan and not bSpeedGreaterThan)
                     local notValidEngineRunning = (data.isEngineRunning and (data.isEngineRunning ~= vehicle:isEngineRunning()))
                     local notValidBraking = (data.isBraking and (data.isBraking ~= vehicle:isBraking()))
-                    local stopSound = data.stop or notValid or notValidEngineRunning or notValidBraking
-                    
+                    local stopSound = data.stopSound or notValid or notValidEngineRunning or notValidBraking
+
                     if stopSound then
-                        if vehicleEmitter:isPlaying(data.sound) then vehicleEmitter:stopSoundByName(data.sound) end
+                        --print(data.sound, ": STOP - ", data.stop, ", ", notValid , ", ",  notValidEngineRunning , ", ",  notValidBraking)
+                        if (not doNotStop) and vehicleEmitter:isPlaying(data.sound) then vehicleEmitter:stopSoundByName(data.sound) end
                     else
+                        --print(data.sound, ": PLAY - ", data.stop, ", ", notValid , ", ",  notValidEngineRunning , ", ",  notValidBraking)
                         if not vehicleEmitter:isPlaying(data.sound) then vehicleEmitter:playAmbientLoopedImpl(data.sound) end
                     end
 
