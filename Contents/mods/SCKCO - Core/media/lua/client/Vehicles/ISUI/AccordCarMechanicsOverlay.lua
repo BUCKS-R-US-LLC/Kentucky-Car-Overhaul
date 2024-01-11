b8a34f27a521d75a7d9e87a3f6b69a8bf7014d98
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2024-01-10 20:41:41 -0500

    Update AccordCarMechanicsOverlay.lua

--- Contents/mods/SCKCO - Core/media/lua/client/Vehicles/ISUI/AccordCarMechanicsOverlay.lua
+++ Contents/mods/SCKCO - Core/media/lua/client/Vehicles/ISUI/AccordCarMechanicsOverlay.lua
@@ -19,4 +19,6 @@ ISCarMechanicsOverlay.PartList["WindshieldRear"].vehicles["accord_"] = {x=0,y=0,
 ISCarMechanicsOverlay.PartList["WindowRearLeft"].vehicles["accord_"] = {x=0,y=0,x2=0,y2=0}
 ISCarMechanicsOverlay.PartList["WindowRearRight"].vehicles["accord_"] = {x=0,y=0,x2=0,y2=0}
 
+ISCarMechanicsOverlay.PartList["TruckBed"].vehicles["accord_"] = {x=0,y=0,x2=0,y2=0}
+
 ISCarMechanicsOverlay.CarList["Base.SmallCar"] = ISCarMechanicsOverlay.CarList["Base.SC_HondaAccord"]
