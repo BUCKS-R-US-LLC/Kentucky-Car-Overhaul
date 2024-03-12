ef0171aafd2b62a9228a14d8dd468be31b713ece
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2024-03-12 19:13:39 -0400

    Distro work

--- Contents/mods/SCKCO - Core/media/lua/server/Vehicles/SCKCO_VehicleDistro.lua
+++ Contents/mods/SCKCO - Core/media/lua/server/Vehicles/SCKCO_VehicleDistro.lua
@@ -5,6 +5,52 @@ VehicleDistributions.SC_CUCV = {
         rolls = 50,
         items = {
 
+            -- MEA -- 
+            "MEA.GIJerryCan", 10,
+            "MEA.AmmoCan556X45", 50,
+            "MEA.AmmoCan762X51", 50,
+            "MEA.AmmoCan50BMG", 10,
+
+        },
+        junk = {
+            rolls = 1,
+            items = {
+
+                "FirstAidKit", 1,
+            }
+        }
+    },
+
+    GloveBox = {
+        rolls = 5,
+        items = {
+
+            "Battery", 10,
+            "HandTorch", 4,
+
+        },
+        junk = {
+            rolls = 1,
+            items = {
+                "HottieZ", 5,
+                "Cigarettes", 8,
+                "Glasses_Aviators", 0.5,
+                "HandTorch", 4,
+                "Lighter", 4,
+            }
+        }
+    },
+
+    SeatRearLeft = VehicleDistributions.Seat;
+    SeatRearRight = VehicleDistributions.Seat;
+}
+
+VehicleDistributions.SC_CUCVMed = {
+    BackOfTruck = {
+        rolls = 50,
+        items = {
+
+            -- MEA --
             "MEA.GIJerryCan", 10,
             "MEA.AmmoCan556X45", 50,
             "MEA.AmmoCan762X51", 50,
@@ -49,6 +95,7 @@ VehicleDistributions.SC_M151 = {
         rolls = 50,
         items = {
 
+            -- MEA -- 
             "MEA.GIJerryCan", 10,
             "MEA.AmmoCan556X45", 50,
             "MEA.AmmoCan762X51", 50,
@@ -73,6 +120,7 @@ VehicleDistributions.SC_Humvee = {
         rolls = 50,
         items = {
 
+            -- MEA -- 
             "MEA.GIJerryCan", 10,
             "MEA.AmmoCan556X45", 50,
             "MEA.AmmoCan762X51", 50,
@@ -104,16 +152,316 @@ VehicleDistributions.SC_Humvee = {
                 "Glasses_Aviators", 0.5,
                 "HandTorch", 4,
                 "Lighter", 4,
-
-                -- Accessories -- 
-
-                "SCA.MREWrapper_SCA", 4,
             }
         }
     },
 
     SeatRearLeft = VehicleDistributions.Seat;
     SeatRearRight = VehicleDistributions.Seat;
+}
+
+VehicleDistributions.SC_Bubba = {
+    TruckBedOpen = {
+        rolls = 50,
+        items = {
+
+            -- SCGL -- 
+            "SCGLClothing.Hat_OrangeHunterTruckerCap", 10,
+            "SCGLClothing.Jacket_M65FieldJacketDuckhunter", 10,
+            "SCGLClothing.Jacket_M65FieldJacketTreebark", 10,
+
+            -- Vanilla -- 
+            "Vest_Hunting_Orange", 10,
+
+        },
+        junk = {
+            rolls = 1,
+            items = {
+
+                "FirstAidKit", 1,
+            }
+        }
+    },
+
+    GloveBox = {
+        rolls = 5,
+        items = {
+
+            "Battery", 10,
+            "HandTorch", 4,
+
+        },
+        junk = {
+            rolls = 1,
+            items = {
+                "HottieZ", 5,
+                "Cigarettes", 8,
+                "HandTorch", 4,
+                "Lighter", 4,
+            }
+        }
+    },
+
+    SeatRearLeft = VehicleDistributions.Seat;
+    SeatRearRight = VehicleDistributions.Seat;
+}
+
+VehicleDistributions.SC_Militia = {
+    TruckBedOpen = {
+        rolls = 50,
+        items = {
+            -- SCGL -- 
+            "SCGLClothing.Hat_OrangeHunterTruckerCap", 10,
+            "SCGLClothing.Jacket_M65FieldJacketDuckhunter", 10,
+            "SCGLClothing.Jacket_M65FieldJacketTreebark", 10,
+
+            -- Vanilla -- 
+            "Vest_Hunting_Orange", 10,
+
+        },
+        junk = {
+            rolls = 1,
+            items = {
+
+                "FirstAidKit", 1,
+            }
+        }
+    },
+
+    GloveBox = {
+        rolls = 5,
+        items = {
+
+            "Battery", 10,
+            "HandTorch", 4,
+
+        },
+        junk = {
+            rolls = 1,
+            items = {
+                "HottieZ", 5,
+                "Cigarettes", 8,
+                "HandTorch", 4,
+                "Lighter", 4,
+            }
+        }
+    },
+
+    SeatRearLeft = VehicleDistributions.Seat;
+    SeatRearRight = VehicleDistributions.Seat;
+}
+
+VehicleDistributions.SC_Cleaners = {
+    TruckBedOpen = {
+        rolls = 10,
+        items = {
+
+            -- SCGL -- 
+            "SCGLClothing.Hat_PASGTHelmetDualTex", 5,
+            "SCGLClothing.Hat_PatrolCapDualTex", 10,
+            "SCGLClothing.Jacket_DualTex", 5,
+            "SCGLClothing.Webbing_LBV88", 10,
+            "SCGLClothing.Vest_AZT", 10,
+            "SCGLClothing.Bag_CFP90PatrolPack", 10,
+        },
+        junk = {
+            rolls = 1,
+            items = {
+
+                "SCGLWeapons.Mask_XM40", 1,
+            }
+        }
+    },
+
+    SeatRearLeft = VehicleDistributions.Seat;
+    SeatRearRight = VehicleDistributions.Seat;
+}
+
+VehicleDistributions.SC_BankCar = {
+    BackOfTruck = {
+        rolls = 10,
+        items = {
+            "Bag_MoneyBag", 100,
+            "Vest_BulletPolice", 10,
+        },
+    },
+
+    SeatRearLeft = VehicleDistributions.Seat;
+    SeatRearRight = VehicleDistributions.Seat;
+}
+
+VehicleDistributions.SC_SWATTruck = {
+    BackOfTruck = {
+        rolls = 50,
+        items = {
+
+            -- SCGL -- 
+            "SCGLClothing.Hat_PASGTHelmetBlack", 5,
+            "SCGLClothing.Vest_HighRiskModular", 5,
+            "SCGLClothing.Mask_MSAPhalanx", 5,
+            "SCGLClothing.Jacket_BlueTactical", 5,
+            "SCGLClothing.Trousers_BlueTactical", 5,
+
+            -- Vanilla -- 
+
+            "Vest_BulletPolice", 10,
+            "Shoes_ArmyBoots", 10,
+        },
+    },
+
+    SeatRearLeft = VehicleDistributions.Seat,
+    SeatRearRight = VehicleDistributions.Seat,
+}
+
+VehicleDistributions.SC_WorkingVan = {
+    TruckBed = {
+        rolls = 60,
+        items = {
+            -- Vanilla -- 
+            "ToolBox", 1,
+            "Bag_ToolBag", 1,
+            "Sledgehammer", 2,
+            "Shovel", 2,
+            "Hat_HardHat", 5,
+        }
+    }, 
+}
+
+VehicleDistributions.SC_McCoy = {
+    TruckBed = {
+        rolls = 60,
+        items = {
+            -- Vanilla -- 
+            "ToolBox", 1,
+            "Bag_ToolBag", 1,
+            "WoodAxe", 2,
+            "Saw", 2,
+            "Hat_HardHat", 5,
+        }
+    }, 
+}
+
+VehicleDistributions.SC_DrunkDriver = {
+    TruckBedOpen = {
+        rolls = 60,
+        items = {
+            -- Vanilla -- 
+            "BeerCanEmpty", 50,
+            "ToolBox", 5,
+        }
+    }, 
+    GloveBox = {
+        rolls = 60,
+        items = {
+            "WhiskeyFull", 10,
+        }
+    }
+}
+
+
+VehicleDistributions.SC_Limo = {
+    LimoInterior = {
+        rolls = 60,
+        items = {
+            -- Vanilla -- 
+            "Wine", 1,
+            "WhiskeyFull", 1,
+            "Bra_Straps_FrillyBlack", 1,
+            "FrillyUnderpants_Black", 1,
+            "Revolver_Short", 1,
+        }
+    }, 
+
+    TruckBed = {
+        rolls = 1,
+        items = {
+
+            "Bag_MoneyBag", 50,
+            "Bag_GolfBag", 5,
+
+        },
+        
+    GloveBox = {
+        rolls = 60,
+        items = {
+            "Bullets38Box", 10,
+        },
+        },
+    }, 
+}
+
+VehicleDistributions.SC_HahnPumper = {
+    SideContainer1 = {
+        rolls = 50,
+        items = {
+            -- SCGL -- 
+            "SCGLWeapons.HalliganBar", 10,
+            "SCGLWeapons.PulaskiAxe", 10,
+            "SCGLClothing.Mask_Scottoramic", 10,
+            "SCGLClothing.AirPakHarness", 10,
+    
+            -- Vanilla -- 
+            "Axe", 10,
+            "Bag_MedicalBag", 10,
+            "Hat_Fireman", 10,
+            "Jacket_Fireman", 10,
+            "Trousers_Fireman", 10,
+            "Shoes_ArmyBoots", 10,
+        },
+    },
+
+    SideContainer2 = {
+        rolls = 100,
+        items = {
+
+            -- SCGL -- 
+            "SCGLWeapons.HalliganBar", 10,
+            "SCGLWeapons.PulaskiAxe", 10,
+            "SCGLClothing.Mask_Scottoramic", 10,
+            "SCGLClothing.AirPakHarness", 10,
+    
+            -- Vanilla -- 
+            "Axe", 10,
+            "Bag_MedicalBag", 10,
+            "Hat_Fireman", 10,
+            "Jacket_Fireman", 10,
+            "Trousers_Fireman", 10,
+            "Shoes_ArmyBoots", 10,
+
+        },
+    },
+
+    SeatRearLeft = VehicleDistributions.Seat,
+    SeatRearRight = VehicleDistributions.Seat,
+}
+
+VehicleDistributions.SC_PlagueCart = {
+    TruckBedOpen = {
+        rolls = 500,
+        items = {
+
+            "SCA.BodyBag", 50,
+            "CorpseMale", 50,
+            "CorpseFemale", 50,
+            "SCA.RedBiohazardBag", 50,
+            "SCA.YellowBiohazardBag", 50,
+
+        },
+    },
+
+    TruckBed = {
+        rolls = 500,
+        items = {
+
+            "SCA.BodyBag", 50,
+            "CorpseMale", 50,
+            "CorpseFemale", 50,
+            "SCA.RedBiohazardBag", 50,
+            "SCA.YellowBiohazardBag", 50,
+
+        },
+    },
+
 }
 
 distributionTable["SC_K5BlazerFossoil"] = distributionTable["CarNormal"]
@@ -131,7 +479,6 @@ distributionTable["SC_JeepCJ7"] = distributionTable["CarNormal"]
 distributionTable["SC_JeepCherokee"] = distributionTable["CarNormal"]
 distributionTable["SC_CrownVictoria92"] = distributionTable["CarNormal"]
 distributionTable["SC_GeoTracker"] = distributionTable["CarNormal"]
-distributionTable["SC_M880Bubba"] = distributionTable["CarNormal"]
 distributionTable["SC_ChevroletC70GrainTruck"] = distributionTable["CarNormal"]
 distributionTable["SC_CapriceCiv"] = distributionTable["CarNormal"]
 distributionTable["SC_StepVanP30Civ"] = distributionTable["CarNormal"]
@@ -148,7 +495,6 @@ distributionTable["SC_Porsche911G50"] = distributionTable["SportsCar"]
 distributionTable["SC_BMW520i"] = distributionTable["SportsCar"]
 distributionTable["SC_CadillacBrougham"] = distributionTable["SportsCar"]
 distributionTable["SC_CadillacFleetwood"] = distributionTable["SportsCar"]
-distributionTable["SC_CadillacLimo"] = distributionTable["SportsCar"]
 
 distributionTable["SC_Caprice9C1StatePolice"] = distributionTable["CarLightsPolice"]
 distributionTable["SC_Caprice9C1LVPD"] = distributionTable["CarLightsPolice"]
@@ -162,14 +508,36 @@ distributionTable["SC_CrownVictoriaJefferson92"] = distributionTable["CarLightsP
 distributionTable["SC_CrownVictoriaWarrena92"] = distributionTable["CarLightsPolice"]
 distributionTable["SC_CrownVictoriaMuhlenberg92"] = distributionTable["CarLightsPolice"]
 
+distributionTable["SC_FordF700ArmoredBank"] = { Normal = VehicleDistributions.SC_BankCar; }
+distributionTable["SC_FordF700ArmoredPolice"] = { Normal = VehicleDistributions.SC_SWATTruck; }
+distributionTable["SC_StepVanP30Police"] = { Normal = VehicleDistributions.SC_SWATTruck; }
+
+distributionTable["SC_HahnPumperRed"] = { Normal = VehicleDistributions.SC_HahnPumper; }
+distributionTable["SC_HahnPumperYellow"] = { Normal = VehicleDistributions.SC_HahnPumper; }
+
+distributionTable["SC_M880Bubba"] = { Normal = VehicleDistributions.SC_Bubba; }
+distributionTable["SC_M880Militia"] = { Normal = VehicleDistributions.SC_Militia; }
+
 distributionTable["SC_M1008"] = { Normal = VehicleDistributions.SC_CUCV; }
 distributionTable["SC_M1009"] = { Normal = VehicleDistributions.SC_CUCV; }
 distributionTable["SC_M1009MilitaryPolice"] = { Normal = VehicleDistributions.SC_CUCV; }
+distributionTable["SC_M1010Ambulance"] = { Normal = VehicleDistributions.SC_CUCVMed; }
+
+distributionTable["SC_CadillacLimo"] = { Normal = VehicleDistributions.SC_Limo; }
 
 distributionTable["SC_M151A2"] = { Normal = VehicleDistributions.SC_M151; }
-distributionTable["SC_M151A2Cleaners"] = { Normal = VehicleDistributions.SC_M151; }
+distributionTable["SC_M151A2Cleaners"] = { Normal = VehicleDistributions.SC_Cleaners; }
 
 distributionTable["SC_M1025"] = { Normal = VehicleDistributions.SC_Humvee; }
 distributionTable["SC_M998"] = { Normal = VehicleDistributions.SC_Humvee; }
 
 distributionTable["SC_M35A1"] = { Normal = VehicleDistributions.SC_M35; }
+
+distributionTable["SC_G30BoxVan"] = { Normal = VehicleDistributions.SC_WorkingVan; }
+distributionTable["SC_G30McCoy"] = { Normal = VehicleDistributions.SC_McCoy; }
+
+distributionTable["SC_K10Dixie"] = { Normal = VehicleDistributions.SC_DrunkDriver; }
+
+distributionTable["SC_FordF700DumpPlagueCart"] = { Normal = VehicleDistributions.SC_PlagueCart; }
+distributionTable["SC_FordF700BoxPlagueCart"] = { Normal = VehicleDistributions.SC_PlagueCart; }
+distributionTable["SC_MackDM800PlagueCart"] = { Normal = VehicleDistributions.SC_PlagueCart; }
