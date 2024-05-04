2b7b48332e5492ceb44f948041b7bbe12d3a6f0c
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2024-05-03 22:30:22 -0400

    Progress

--- Contents/mods/SCKCO - Core/media/lua/client/Vehicles/ISUI/AMGeneralCarMechanicsOverlay.lua
+++ Contents/mods/SCKCO - Core/media/lua/client/Vehicles/ISUI/AMGeneralCarMechanicsOverlay.lua
@@ -1,5 +1,9 @@
-ISCarMechanicsOverlay.CarList["Base.SC_MazdaB2000"] = {imgPrefix = "mazdaB_", x=0,y=0};
---
+ISCarMechanicsOverlay.CarList["Base.SC_M1025"] = {imgPrefix = "m1025_", x=0,y=0};
+ISCarMechanicsOverlay.CarList["Base.SC_M998"] = {imgPrefix = "m998_", x=0,y=0};
+ISCarMechanicsOverlay.CarList["Base.SC_M35A1"] = {imgPrefix = "M35A1_", x=0,y=0};
+ISCarMechanicsOverlay.CarList["Base.SC_M151A2"] = {imgPrefix = "m151_", x=0,y=0};
+ISCarMechanicsOverlay.CarList["Base.SC_M151A2Cleaners"] = {imgPrefix = "m151_", x=0,y=0};
+
 ISCarMechanicsOverlay.PartList["Battery"].vehicles = ISCarMechanicsOverlay.PartList["Battery"].vehicles or {};
 ISCarMechanicsOverlay.PartList["Battery"].vehicles["all_"] = {img="battery", x=0,y=0,x2=0,y2=0};
 --
