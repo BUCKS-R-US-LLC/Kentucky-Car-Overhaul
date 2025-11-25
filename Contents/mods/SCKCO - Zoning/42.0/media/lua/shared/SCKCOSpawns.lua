abf7e1de2c49fd30bde748859077d08e185ec494
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2025-11-24 23:19:23 -0500

    Revert "Removed zoning, now just one mod"
    This reverts commit 9bc359bfa1db0872c54e7cdccbea942905ce18f9.

---
+++ Contents/mods/SCKCO - Zoning/42.0/media/lua/shared/SCKCOSpawns.lua
@@ -0,0 +1,208 @@
+if VehicleZoneDistribution then
+
+	VehicleZoneDistribution.military_general = VehicleZoneDistribution.military_general or {};
+	VehicleZoneDistribution.military_general.vehicles = VehicleZoneDistribution.military_general.vehicles or {};
+
+	VehicleZoneDistribution.military_general.vehicles["Base.SC_DodgeM880"] = {index = -1, spawnChance = 5};
+	VehicleZoneDistribution.military_general.vehicles["Base.SC_AMGeneralM151A2"] = {index = -1, spawnChance = 20};
+	VehicleZoneDistribution.military_general.vehicles["Base.SC_AMGeneralM998"] = {index = -1, spawnChance = 50};
+	VehicleZoneDistribution.military_general.vehicles["Base.SC_AMGeneralM1025"] = {index = -1, spawnChance = 30};
+	VehicleZoneDistribution.military_general.vehicles["Base.SC_ChevroletM1009_3Tone"] = {index = -1, spawnChance = 10};
+	VehicleZoneDistribution.military_general.vehicles["Base.SC_ChevroletM1008_3Tone"] = {index = -1, spawnChance = 10};
+	VehicleZoneDistribution.military_general.vehicles["Base.SC_ChevroletM1028A3_3Tone"] = {index = -1, spawnChance = 10};
+	VehicleZoneDistribution.military_general.vehicles["Base.SC_ChevroletM1008TroopCarrier_3Tone"] = {index = -1, spawnChance = 70};
+	VehicleZoneDistribution.military_general.vehicles["Base.SC_ChevroletM1010Ambulance_3Tone"] = {index = -1, spawnChance = 20};
+	VehicleZoneDistribution.military_general.vehicles["Base.SC_AMGeneralM35A2_3Tone"] = {index = -1, spawnChance = 30};
+
+	VehicleZoneDistribution.military_general.baseVehicleQuality = 1.1;
+	VehicleZoneDistribution.military_general.chanceToSpawnKey = 80;
+	VehicleZoneDistribution.military_general.chanceToSpawnSpecial = 0;
+	VehicleZoneDistribution.military_general.spawnRate = 100;
+
+	VehicleZoneDistribution.military_utility = VehicleZoneDistribution.military_utility or {};
+	VehicleZoneDistribution.military_utility.vehicles = VehicleZoneDistribution.military_utility.vehicles or {};
+
+	VehicleZoneDistribution.military_utility.vehicles["Base.SC_DodgeM880"] = {index = -1, spawnChance = 10};
+	VehicleZoneDistribution.military_utility.vehicles["Base.SC_AMGeneralM151A2_3Tone"] = {index = -1, spawnChance = 20};
+	VehicleZoneDistribution.military_utility.vehicles["Base.SC_ChevroletM1009_3Tone"] = {index = -1, spawnChance = 60};
+	VehicleZoneDistribution.military_utility.vehicles["Base.SC_ChevroletM1008_3Tone"] = {index = -1, spawnChance = 60};
+	VehicleZoneDistribution.military_utility.vehicles["Base.SC_ChevroletM1028A3_Tone"] = {index = -1, spawnChance = 60};
+	VehicleZoneDistribution.military_utility.vehicles["Base.SC_ChevroletM1008TroopCarrier_3Tone"] = {index = -1, spawnChance = 70};
+	VehicleZoneDistribution.military_utility.vehicles["Base.SC_ChevroletM1010Ambulance_3Tone"] = {index = -1, spawnChance = 20};
+	VehicleZoneDistribution.military_utility.vehicles["Base.SC_CadillacGageRangerLights"] = {index = -1, spawnChance = 5};
+
+	VehicleZoneDistribution.military_utility.baseVehicleQuality = 1.1;
+	VehicleZoneDistribution.military_utility.chanceToSpawnKey = 80;
+	VehicleZoneDistribution.military_utility.chanceToSpawnSpecial = 0;
+	VehicleZoneDistribution.military_utility.spawnRate = 100;
+	
+	VehicleZoneDistribution.military_heavyvehicles = VehicleZoneDistribution.military_heavyvehicles or {};
+	VehicleZoneDistribution.military_heavyvehicles.vehicles = VehicleZoneDistribution.military_heavyvehicles.vehicles or {};
+
+	VehicleZoneDistribution.military_heavyvehicles.vehicles["Base.SC_AMGeneralM35A2_3Tone"] = {index = -1, spawnChance = 100};
+
+	VehicleZoneDistribution.military_heavyvehicles.baseVehicleQuality = 1.1;
+	VehicleZoneDistribution.military_heavyvehicles.chanceToSpawnKey = 80;
+	VehicleZoneDistribution.military_heavyvehicles.chanceToSpawnSpecial = 0;
+	VehicleZoneDistribution.military_heavyvehicles.spawnRate = 100;
+
+	VehicleZoneDistribution.military_police = VehicleZoneDistribution.military_police or {};
+	VehicleZoneDistribution.military_police.vehicles = VehicleZoneDistribution.military_police.vehicles or {};
+
+	VehicleZoneDistribution.military_police.vehicles["Base.SC_AMGeneralM1025MP"] = {index = -1, spawnChance = 30};
+	VehicleZoneDistribution.military_police.vehicles["Base.SC_AMGeneralM1009MP_3Tone"] = {index = -1, spawnChance = 70};
+	VehicleZoneDistribution.military_police.vehicles["Base.SC_CadillacGageRangerLights"] = {index = -1, spawnChance = 30};
+
+	VehicleZoneDistribution.military_police.baseVehicleQuality = 1.1;
+	VehicleZoneDistribution.military_police.chanceToSpawnKey = 80;
+	VehicleZoneDistribution.military_police.chanceToSpawnSpecial = 0;
+	VehicleZoneDistribution.military_police.spawnRate = 100;
+
+	VehicleZoneDistribution.louisville_police = VehicleZoneDistribution.louisville_police or {};
+	VehicleZoneDistribution.louisville_police.vehicles = VehicleZoneDistribution.louisville_police.vehicles or {};
+
+	VehicleZoneDistribution.louisville_police.vehicles["Base.SC_FordCrownVictoria85LVPD"] = {index = -1, spawnChance = 90};
+	VehicleZoneDistribution.louisville_police.vehicles["Base.SC_JeepCherokeeAudubon"] = {index = -1, spawnChance = 20};
+	VehicleZoneDistribution.louisville_police.vehicles["Base.SC_FordMustang90JeffersonCounty"] = {index = -1, spawnChance = 40};
+	VehicleZoneDistribution.louisville_police.vehicles["Base.SC_FordMustang90Undercover"] = {index = -1, spawnChance = 20};
+
+	VehicleZoneDistribution.louisville_police.baseVehicleQuality = 1.1;
+	VehicleZoneDistribution.louisville_police.spawnRate = 75;
+	
+	VehicleZoneDistribution.louisville_fire_engines = VehicleZoneDistribution.louisville_fire_engines or {};
+	VehicleZoneDistribution.louisville_fire_engines.vehicles = VehicleZoneDistribution.louisville_fire_engines.vehicles or {};
+
+	VehicleZoneDistribution.louisville_fire_engines.vehicles["Base.SC_CrownFireCoach_Red"] = {index = -1, spawnChance = 40};
+	VehicleZoneDistribution.louisville_fire_engines.vehicles["Base.SC_CrownFireCoach_White"] = {index = -1, spawnChance = 40};
+	VehicleZoneDistribution.louisville_fire_engines.vehicles["Base.SC_HahnPumper_Red"] = {index = -1, spawnChance = 80};
+	VehicleZoneDistribution.louisville_fire_engines.vehicles["Base.SC_HahnPumper_Yellow"] = {index = -1, spawnChance = 80};
+
+	VehicleZoneDistribution.louisville_fire_engines.baseVehicleQuality = 1.1;
+	VehicleZoneDistribution.louisville_fire_engines.chanceToSpawnKey = 80;
+	VehicleZoneDistribution.louisville_fire_engines.chanceToSpawnSpecial = 0;
+	VehicleZoneDistribution.louisville_fire_engines.spawnRate = 75;
+
+	VehicleZoneDistribution.national_park_service = VehicleZoneDistribution.national_park_service or {};
+	VehicleZoneDistribution.national_park_service.vehicles = VehicleZoneDistribution.national_park_service.vehicles or {};
+
+	VehicleZoneDistribution.national_park_service.vehicles["Base.SC_ChevroletCaprice85ParkRanger"] = {index = -1, spawnChance = 20};
+
+	VehicleZoneDistribution.national_park_service.baseVehicleQuality = 1.1;
+	VehicleZoneDistribution.national_park_service.chanceToSpawnKey = 80;
+	VehicleZoneDistribution.national_park_service.chanceToSpawnSpecial = 0;
+	VehicleZoneDistribution.national_park_service.spawnRate = 75;
+
+	VehicleZoneDistribution.construction_site = VehicleZoneDistribution.construction_site or {};
+	VehicleZoneDistribution.construction_site.vehicles = VehicleZoneDistribution.construction_site.vehicles or {};
+
+	VehicleZoneDistribution.construction_site.vehicles["Base.SC_ChevroletG30Mccoy"] = {index = -1, spawnChance = 20};
+	VehicleZoneDistribution.construction_site.vehicles["Base.SC_ChevroletG30BoxVan"] = {index = -1, spawnChance = 80};
+
+	VehicleZoneDistribution.construction_site.baseVehicleQuality = 1.1;
+	VehicleZoneDistribution.construction_site.chanceToSpawnKey = 80;
+	VehicleZoneDistribution.construction_site.chanceToSpawnSpecial = 0;
+	VehicleZoneDistribution.construction_site.spawnRate = 75;
+
+	VehicleZoneDistribution.farming_machinery = VehicleZoneDistribution.farming_machinery or {};
+	VehicleZoneDistribution.farming_machinery.vehicles = VehicleZoneDistribution.farming_machinery.vehicles or {};
+
+	VehicleZoneDistribution.farming_machinery.vehicles["Base.SC_Oliver2050"] = {index = -1, spawnChance = 90};
+	VehicleZoneDistribution.farming_machinery.vehicles["Base.SC_SmallLivestockTrailer"] = {index = -1, spawnChance = 50};
+
+	VehicleZoneDistribution.farming_machinery.baseVehicleQuality = 1.1;
+	VehicleZoneDistribution.farming_machinery.chanceToSpawnKey = 80;
+	VehicleZoneDistribution.farming_machinery.chanceToSpawnSpecial = 0;
+	VehicleZoneDistribution.farming_machinery.spawnRate = 55;
+	
+	VehicleZoneDistribution.designated_bubba_zone = VehicleZoneDistribution.designated_bubba_zone or {};
+	VehicleZoneDistribution.designated_bubba_zone.vehicles = VehicleZoneDistribution.designated_bubba_zone.vehicles or {};
+
+	VehicleZoneDistribution.designated_bubba_zone.vehicles["Base.SC_DodgeM880Bubba"] = {index = -1, spawnChance = 60};
+	VehicleZoneDistribution.designated_bubba_zone.vehicles["Base.SC_ChevroletC70Survivalist"] = {index = -1, spawnChance = 5};
+	VehicleZoneDistribution.designated_bubba_zone.vehicles["Base.SC_CUCVZhenjaEdition"] = {index = -1, spawnChance = 5};
+	VehicleZoneDistribution.designated_bubba_zone.vehicles["Base.SC_ChevroletK10Dixie"] = {index = -1, spawnChance = 5};
+
+	VehicleZoneDistribution.designated_bubba_zone.baseVehicleQuality = 1.1;
+	VehicleZoneDistribution.designated_bubba_zone.chanceToSpawnKey = 80;
+	VehicleZoneDistribution.designated_bubba_zone.chanceToSpawnSpecial = 0;
+	VehicleZoneDistribution.designated_bubba_zone.spawnRate = 55;
+	
+	VehicleZoneDistribution.cemetery = VehicleZoneDistribution.cemetery or {};
+	VehicleZoneDistribution.cemetery.vehicles = VehicleZoneDistribution.cemetery.vehicles or {};
+
+	VehicleZoneDistribution.cemetery.vehicles["Base.SC_CadillacFuneralCoach"] = {index = -1, spawnChance = 80};
+
+	VehicleZoneDistribution.cemetery.baseVehicleQuality = 1.1;
+	VehicleZoneDistribution.cemetery.chanceToSpawnKey = 80;
+	VehicleZoneDistribution.cemetery.chanceToSpawnSpecial = 0;
+	VehicleZoneDistribution.cemetery.spawnRate = 75;
+
+	VehicleZoneDistribution.military_police_medical = VehicleZoneDistribution.military_police_medical or {};
+	VehicleZoneDistribution.military_police_medical.vehicles = VehicleZoneDistribution.military_police_medical.vehicles or {};
+
+	VehicleZoneDistribution.military_police_medical.vehicles["Base.SC_FordF350Ambulance"] = {index = -1, spawnChance = 10};
+	VehicleZoneDistribution.military_police_medical.vehicles["Base.SC_HahnPumper_Red"] = {index = -1, spawnChance = 10};
+	VehicleZoneDistribution.military_police_medical.vehicles["Base.SC_HahnPumper_Yellow"] = {index = -1, spawnChance = 10};
+	VehicleZoneDistribution.military_police_medical.vehicles["Base.SC_FordCrownVictoriaLouisville92"] = {index = -1, spawnChance = 10};
+	VehicleZoneDistribution.military_police_medical.vehicles["Base.SC_FordCrownVictoriaJefferson92"] = {index = -1, spawnChance = 10};
+	VehicleZoneDistribution.military_police_medical.vehicles["Base.SC_DodgeM880"] = {index = -1, spawnChance = 5};
+	VehicleZoneDistribution.military_police_medical.vehicles["Base.SC_AMGeneralM1025"] = {index = -1, spawnChance = 40};
+	VehicleZoneDistribution.military_police_medical.vehicles["Base.SC_AMGeneralM1025MP"] = {index = -1, spawnChance = 20};
+	VehicleZoneDistribution.military_police_medical.vehicles["Base.SC_AMGeneralM998"] = {index = -1, spawnChance = 70};
+	VehicleZoneDistribution.military_police_medical.vehicles["Base.SC_ChevroletCaprice85StatePolice"] = {index = -1, spawnChance = 40};
+	VehicleZoneDistribution.military_police_medical.vehicles["Base.SC_ChevroletM1009_3Tone"] = {index = -1, spawnChance = 20};
+	VehicleZoneDistribution.military_police_medical.vehicles["Base.SC_ChevroletM1009MP_3Tone"] = {index = -1, spawnChance = 20};
+	VehicleZoneDistribution.military_police_medical.vehicles["Base.SC_ChevroletM1010Ambulance"] = {index = -1, spawnChance = 40};
+	VehicleZoneDistribution.military_police_medical.vehicles["Base.SC_AMGeneralM35A2_3Tone"] = {index = -1, spawnChance = 15};
+	VehicleZoneDistribution.military_police_medical.vehicles["Base.SC_CadillacGageRanger_3Tone"] = {index = -1, spawnChance = 5};
+
+	VehicleZoneDistribution.military_police_medical.baseVehicleQuality = 1.1;
+	VehicleZoneDistribution.military_police_medical.chanceToSpawnKey = 80;
+	VehicleZoneDistribution.military_police_medical.chanceToSpawnSpecial = 0;
+	VehicleZoneDistribution.military_police_medical.spawnRate = 75;
+	
+	VehicleZoneDistribution.police_tactical_vehicles = VehicleZoneDistribution.police_tactical_vehicles or {};
+	VehicleZoneDistribution.police_tactical_vehicles.vehicles = VehicleZoneDistribution.police_tactical_vehicles.vehicles or {};
+
+	VehicleZoneDistribution.police_tactical_vehicles.vehicles["Base.SC_ChevroletP30SWAT"] = {index = -1, spawnChance = 50};
+	VehicleZoneDistribution.police_tactical_vehicles.vehicles["Base.SC_FordF700Police"] = {index = -1, spawnChance = 50};
+
+	VehicleZoneDistribution.police_tactical_vehicles.baseVehicleQuality = 1.1;
+	VehicleZoneDistribution.police_tactical_vehicles.chanceToSpawnKey = 80;
+	VehicleZoneDistribution.police_tactical_vehicles.chanceToSpawnSpecial = 0;
+	VehicleZoneDistribution.police_tactical_vehicles.spawnRate = 70;
+
+	VehicleZoneDistribution.old_money = VehicleZoneDistribution.old_money or {};
+	VehicleZoneDistribution.old_money.vehicles = VehicleZoneDistribution.old_money.vehicles or {};
+	
+	VehicleZoneDistribution.old_money.vehicles["Base.SC_CadillacFleetwoodBrougham"] = {index = -1, spawnChance = 20};
+	VehicleZoneDistribution.old_money.vehicles["Base.SC_CadillacLimo"] = {index = -1, spawnChance = 20};
+	
+	VehicleZoneDistribution.old_money.baseVehicleQuality = 1.1;
+	VehicleZoneDistribution.old_money.chanceToSpawnKey = 80;
+	VehicleZoneDistribution.old_money.chanceToSpawnSpecial = 0;
+	VehicleZoneDistribution.old_money.spawnRate = 100;
+
+	VehicleZoneDistribution.new_money = VehicleZoneDistribution.new_money or {};
+	VehicleZoneDistribution.new_money.vehicles = VehicleZoneDistribution.new_money.vehicles or {};
+	
+	VehicleZoneDistribution.new_money.vehicles["Base.SC_Porsche911"] = {index = -1, spawnChance = 50};
+	VehicleZoneDistribution.new_money.vehicles["Base.SC_BMW520i"] = {index = -1, spawnChance = 20};
+	
+	VehicleZoneDistribution.new_money.baseVehicleQuality = 1.1;
+	VehicleZoneDistribution.new_money.chanceToSpawnKey = 80;
+	VehicleZoneDistribution.new_money.chanceToSpawnSpecial = 0;
+	VehicleZoneDistribution.new_money.spawnRate = 100;
+
+	VehicleZoneDistribution.secret_lab_wrecks = VehicleZoneDistribution.secret_lab_wrecks or {};
+    VehicleZoneDistribution.secret_lab_wrecks.vehicles = VehicleZoneDistribution.secret_lab_wrecks.vehicles or {};
+
+    VehicleZoneDistribution.secret_lab_wrecks.vehicles["Base.SC_AMGeneralM35A2"] = {index = -1, spawnChance = 10};
+	VehicleZoneDistribution.secret_lab_wrecks.vehicles["Base.SC_AMGeneralM151A2Cleaners"] = {index = -1, spawnChance = 30};
+	
+    VehicleZoneDistribution.secret_lab_wrecks.baseVehicleQuality = 1.1;
+    VehicleZoneDistribution.secret_lab_wrecks.chanceToSpawnKey = 80;
+    VehicleZoneDistribution.secret_lab_wrecks.chanceToSpawnSpecial = 0;
+    VehicleZoneDistribution.secret_lab_wrecks.spawnRate = 70;
+
+end
---
+++ Contents/mods/SCKCO - Zoning/42.0/media/maps/SCKCOZones/objects.lua
@@ -0,0 +1,35 @@
+objects = {
+
+  { name = "military_general", type = "ParkingStall", x = 12515, y = 4223, z = 0, width = 5, height = 3 },
+
+  { name = "military_utility", type = "ParkingStall", x = 12603, y = 4365, z = 0, width = 5, height = 3 },
+
+  { name = "military_heavyvehicles", type = "ParkingStall", x = 12558, y = 1150, z = 0, width = 5, height = 3 },
+
+  { name = "military_police", type = "ParkingStall", x = 10354, y = 12405, z = 0, width = 5, height = 3 },
+
+  { name = "military_police_medical", type = "ParkingStall", x = 12490, y = 4041, z = 0, width = 5, height = 3 },
+
+  { name = "national_park_service", type = "ParkingStall", x = 4834, y = 6289, z = 0, width = 5, height = 3 },
+
+  { name = "louisville_police", type = "ParkingStall", x = 12465, y = 3661, z = 0, width = 5, height = 3 },
+
+  { name = "police_tactical_vehicles", type = "ParkingStall", x = 8067, y = 11750, z = 0, width = 5, height = 3 },
+
+  { name = "louisville_fire_engines", type = "ParkingStall", x = 12569, y = 1136, z = 0, width = 5, height = 3 },
+
+  { name = "rosewood_fire_engines", type = "ParkingStall", x = 8149, y = 11753, z = 0, width = 5, height = 3 },
+
+  { name = "construction_site", type = "ParkingStall", x = 13769, y = 1626, z = 0, width = 5, height = 3 },
+
+  { name = "designated_bubba_zone", type = "ParkingStall", x = 13266, y = 5462, z = 0, width = 5, height = 3 },
+
+  { name = "old_money", type = "ParkingStall", x = 5267, y = 6477, z = 0, width = 5, height = 3 },
+
+  { name = "new_money", type = "ParkingStall", x = 6116, y = 6726, z = 0, width = 5, height = 3 },
+  
+  { name = "cemetery", type = "ParkingStall", x = 12620, y = 3367, z = 0, width = 5, height = 3 },
+
+  { name = "secret_lab_wrecks", type = "ParkingStall", x = 5860, y = 12480, z = 0, width = 5, height = 3 },
+
+}
